cbd1c6e2698c0b45626c2651b3dbaca64a3d30db
smahaffey9876 <smahaffey9876@hotmail.com>
2016-03-27 12:31:05 -0400

    Updates for accel set pt from joystick for initial testing
    Conflicts:
    	riptide_navigation/CMakeLists.txt~

---
+++ riptide_navigation/src/accel_set.cpp
@@ -0,0 +1,55 @@
+#include "ros/ros.h"
+#include "sensor_msgs/Joy.h"
+#include "geometry_msgs/AccelStamped.h"
+
+class Accel
+{
+  private:
+    ros::NodeHandle nh;
+    ros::Publisher accels;
+    ros::Subscriber js;
+    geometry_msgs::AccelStamped accel;
+
+  public:
+    Accel();
+    void joy_callback(const sensor_msgs::Joy::ConstPtr& joy);
+    void loop();
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "joy_accel_set");
+  Accel accel;
+  accel.loop();
+}
+
+Accel::Accel()
+{
+  js = nh.subscribe<sensor_msgs::Joy>("joy", 1, &Accel::joy_callback, this);
+  accels = nh.advertise<geometry_msgs::AccelStamped>("accel_set_pt", 1);
+}
+
+void Accel::joy_callback(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  ros::Time time = ros::Time::now();
+  accel.header.stamp = time;
+  accel.accel.linear.x = 5 * joy->axes[1];
+  accel.accel.linear.y = 0;
+  accel.accel.linear.z = 5 * (joy->axes[14] - joy->axes[15]);
+
+  accel.accel.angular.x = 5 * 2 * 3.14159 * joy->axes[2] * -1;
+  accel.accel.angular.y = 5 * 2 * 3.14159 * joy->axes[3];
+  accel.accel.angular.z = 5 * 2 * 3.14159 * (joy->axes[13] - joy->axes[12]);
+
+  accels.publish(accel);
+}
+
+void Accel::loop()
+{
+  ros::Rate rate(30);
+  while(ros::ok())
+  {
+    ros::spinOnce();
+    rate.sleep();
+  }
+}
