1821f3732d6a350069bdfceba1fd5d08f6653486
smahaffey9876 <smahaffey9876@hotmail.com>
2016-04-07 19:06:08 -0400

    Working code for controller through PWM

--- riptide_navigation/src/accel_set.cpp
+++ riptide_navigation/src/accel_set.cpp
@@ -1,14 +1,16 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
 #include "geometry_msgs/AccelStamped.h"
+#include "geometry_msgs/Vector3Stamped.h"
 
 class Accel
 {
   private:
     ros::NodeHandle nh;
-    ros::Publisher accels;
+    ros::Publisher accels, orientation;
     ros::Subscriber js;
     geometry_msgs::AccelStamped accel;
+    geometry_msgs::Vector3Stamped orient;
 
   public:
     Accel();
@@ -27,6 +29,7 @@ Accel::Accel()
 {
   js = nh.subscribe<sensor_msgs::Joy>("joy", 1, &Accel::joy_callback, this);
   accels = nh.advertise<geometry_msgs::AccelStamped>("accel_set_pt", 1);
+  orientation = nh.advertise<geometry_msgs::Vector3Stamped>("orientation_set_pt",1);
 }
 
 void Accel::joy_callback(const sensor_msgs::Joy::ConstPtr& joy)
@@ -37,11 +40,15 @@ void Accel::joy_callback(const sensor_msgs::Joy::ConstPtr& joy)
   accel.accel.linear.y = 0;
   accel.accel.linear.z = 5 * (joy->axes[14] - joy->axes[15]);
 
-  accel.accel.angular.x = 5 * 2 * 3.14159 * joy->axes[2] * -1;
-  accel.accel.angular.y = 5 * 2 * 3.14159 * joy->axes[3];
-  accel.accel.angular.z = 5 * 2 * 3.14159 * (joy->axes[13] - joy->axes[12]);
+  ros::Time timestamp = ros::Time::now();
+  orient.header.stamp = timestamp;
+
+  orient.vector.x = 5 * 2 * 3.14159 * joy->axes[2] * -1;
+  orient.vector.y = 5 * 2 * 3.14159 * joy->axes[3];
+  orient.vector.z = 5 * 2 * 3.14159 * (joy->axes[13] - joy->axes[12]);
 
   accels.publish(accel);
+  orientation.publish(orient);
 }
 
 void Accel::loop()
