152464ae03a63cdb9253625d50ecb2ad098b4ae3
smahaffey9876 <smahaffey9876@hotmail.com>
2016-04-07 19:06:08 -0400

    Working code for controller through PWM
    Conflicts:
    	riptide_navigation/CMakeLists.txt
    	riptide_navigation/CMakeLists.txt~
    	riptide_navigation/src/accel_set.cpp~
    	riptide_navigation/src/thrust_mapper.cpp

--- riptide_navigation/src/pid_linear.cpp
+++ riptide_navigation/src/pid_linear.cpp
@@ -2,6 +2,7 @@
 #include "geometry_msgs/Accel.h"
 #include "geometry_msgs/AccelStamped.h"
 #include <std_msgs/Float64.h>
+#include "geometry_msgs/Vector3Stamped.h"
 #include <sensor_msgs/Imu.h>
 #include <control_toolbox/pid.h>
 #include <message_filters/subscriber.h>
@@ -33,11 +34,11 @@ im=config.im;
 dm=config.dm;
 }
 
-void callback(const sensor_msgs::Imu::ConstPtr& current_accel, const geometry_msgs::AccelStamped::ConstPtr& accel_set)
+void callback(const sensor_msgs::Imu::ConstPtr& current_accel, const geometry_msgs::AccelStamped::ConstPtr& accel_set,const geometry_msgs::Vector3Stamped::ConstPtr& accel_roll,const geometry_msgs::Vector3Stamped::ConstPtr& accel_pitch,const geometry_msgs::Vector3Stamped::ConstPtr& accel_yaw)
 {
 
-  geometry_msgs::Vector3 accel_des,accel_thrusters;
-
+  geometry_msgs::Vector3 accel_des;
+  geometry_msgs::Accel accel_thrusters;
   ros::Time time;
   ros::Duration time_diff;
   ros::Time last_time;
@@ -59,17 +60,22 @@ void callback(const sensor_msgs::Imu::ConstPtr& current_accel, const geometry_ms
 
   time = ros::Time::now();
   time_diff = time-last_time;
-  accel_thrusters.x=pid.control_toolbox::Pid::computeCommand((accel_des.x-currentx),time_diff);
+  accel_thrusters.linear.x=pid.control_toolbox::Pid::computeCommand((accel_des.x-currentx),time_diff);
 
-  accel_thrusters.y=pid.control_toolbox::Pid::computeCommand((accel_des.y-currenty),time_diff);
+  accel_thrusters.linear.y=pid.control_toolbox::Pid::computeCommand((accel_des.y-currenty),time_diff);
 
-  accel_thrusters.z=pid.control_toolbox::Pid::computeCommand((accel_des.z-currentz),time_diff);
+  accel_thrusters.linear.z=pid.control_toolbox::Pid::computeCommand((accel_des.z-currentz),time_diff);
 
   pid.control_toolbox::Pid::getCurrentCmd();
   last_time = time;
-  ROS_INFO("PID Accel Results -> X:%f   Y:%f   Z:%f",accel_thrusters.x,accel_thrusters.y,accel_thrusters.z);
 
-a_e.publish(accel_thrusters);
+  accel_thrusters.angular.x=accel_roll->vector.x;
+  accel_thrusters.angular.y=accel_pitch->vector.y;
+  accel_thrusters.angular.z=accel_yaw->vector.z;
+
+  ROS_INFO("PID Accel Results -> X:%f   Y:%f   Z:%f",accel_thrusters.linear.x,accel_thrusters.linear.y,accel_thrusters.linear.z);
+  
+  a_e.publish(accel_thrusters);
 }
 
 int main(int argc, char **argv) {
@@ -84,22 +90,22 @@ int main(int argc, char **argv) {
   node_priv.param<double>("im",im,.3);
   node_priv.param<double>("dm",dm,-.3);
 
-  geometry_msgs::Accel accels_err;
-
   message_filters::Subscriber<sensor_msgs::Imu> imu_sub(nh, "/imu/imu",1);
   message_filters::Subscriber<geometry_msgs::AccelStamped> accel_sub(nh, "accel_set_pt",1);
+  message_filters::Subscriber<geometry_msgs::Vector3Stamped> roll_sub(nh,"accel_error_roll",1);
+  message_filters::Subscriber<geometry_msgs::Vector3Stamped> pitch_sub(nh,"accel_error_pitch",1);
+  message_filters::Subscriber<geometry_msgs::Vector3Stamped> yaw_sub(nh,"accel_error_yaw",1);
 
   dynamic_reconfigure::Server<riptide_navigation::pidConfig> server;
   dynamic_reconfigure::Server<riptide_navigation::pidConfig>::CallbackType f;
   f = boost::bind(&dyn_callback, _1, _2);
   server.setCallback(f);
 
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Imu, geometry_msgs::AccelStamped,geometry_msgs::Vector3Stamped,geometry_msgs::Vector3Stamped,geometry_msgs::Vector3Stamped> MySyncPolicy;
+    message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(100), imu_sub, accel_sub, roll_sub, pitch_sub, yaw_sub);  
+    sync.registerCallback(boost::bind(&callback,_1,_2,_3,_4,_5));
 
-  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Imu, geometry_msgs::AccelStamped> MySyncPolicy;
-    message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(100), imu_sub, accel_sub);  
-    sync.registerCallback(boost::bind(&callback,_1,_2));
-
-    a_e = nh.advertise<geometry_msgs::Vector3>("accel_error", 1);
+    a_e = nh.advertise<geometry_msgs::Accel>("accel_error", 1);
   ros::spin();
 
 }
