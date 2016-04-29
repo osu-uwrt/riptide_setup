84224e89a4e0a54d4ee37139c834ab8ffaf111e3
Conor Cary <cary.42@osu.edu>
2016-04-29 18:53:53 -0400

    Revert "Testing alternative theme installation method."
    This reverts commit 8a334e11bbf316c38a7b63b5e09f1451fd835e2b.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@
 
 import sys
 import os
+# To use the RTD Theme locally:
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -115,7 +117,7 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["themes"]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
