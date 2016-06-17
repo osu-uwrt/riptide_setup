fcddb5e1c558928df3504f2fa224d9b2dbec2138
Conor Cary <cary.42@osu.edu>
2016-06-16 23:52:26 -0400

    Rewrite thruster calibration.

--- riptide_hardware/src/thrust_cal.cpp
+++ riptide_hardware/src/thrust_cal.cpp
@@ -6,77 +6,77 @@ class ThrustCal
 {
   private:
     ros::NodeHandle nh;
-    ros::Subscriber ts;
+    ros::Subscriber thrust;
     ros::Publisher pwm;
-    riptide_msgs::PwmStamped duration;
-    double max_force;
-    double max_pwm;
-    double f1_fwd,f2_fwd,f3_fwd,f4_fwd;
-    double f1_rev,f2_rev,f3_rev,f4_rev;
-    double u1_fwd,u2_fwd,u3_fwd,u4_fwd;
-    double u1_rev,u2_rev,u3_rev,u4_rev;
-    double s1_fwd,s2_fwd;
-    double s1_rev,s2_rev;
-    int calibrate(double raw_force, double fwd_cal, double rev_cal);
+    riptide_msgs::PwmStamped us;
+    double s_p_h_fw, s_p_h_rv;
+    double s_s_h_fw, s_s_h_rv;
+    double s_p_l_fw, s_p_l_rv;
+    double s_s_l_fw, s_s_l_rv;
+    double swa_f_fw, swa_f_rv;
+    double swa_a_fw, swa_a_rv;
+    double h_p_f_fw, h_p_f_rv;
+    double h_s_f_fw, h_s_f_rv;
+    double h_p_a_fw, h_p_a_rv;
+    double h_s_a_fw, h_s_a_rv;
+    int cal(double raw_force, double fwd_scale, double rev_scale);
 
   public:
     ThrustCal();
-    void callback(const riptide_msgs::ThrustStamped::ConstPtr& force);
+    void callback(const riptide_msgs::ThrustStamped::ConstPtr& thrust);
     void loop();
 };
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "thrust_cal");
+  ros::init(argc, argv, "thruster_calibration");
   ThrustCal thrust_cal;
   thrust_cal.loop();
 }
 
 ThrustCal::ThrustCal() : nh()
 {
-  ts = nh.subscribe<riptide_msgs::ThrustStamped>("solver/thrust", 1, &ThrustCal::callback, this);
-  pwm = nh.advertise<riptide_msgs::PwmStamped>("thrust_cal/pwm", 1);
+  thrust = nh.subscribe<riptide_msgs::ThrustStamped>("command/thrust", 1, &ThrustCal::callback, this);
+  pwm = nh.advertise<riptide_msgs::PwmStamped>("command/pwm", 1);
 
-  nh.param<double>("nominal_max_force", max_force, 25.0);
-  nh.param<double>("nominal_max_pwm", max_pwm, 100.0);
-  nh.param<double>("thrust_cal/f1_fwd", f1_fwd, 1.0);
-  nh.param<double>("thrust_cal/f1_rev", f1_rev, 1.0);
-  nh.param<double>("thrust_cal/f2_fwd", f2_fwd, 1.0);
-  nh.param<double>("thrust_cal/f2_rev", f2_rev, 1.0);
-  nh.param<double>("thrust_cal/f3_fwd", f3_fwd, 1.0);
-  nh.param<double>("thrust_cal/f3_rev", f3_rev, 1.0);
-  nh.param<double>("thrust_cal/f4_fwd", f4_fwd, 1.0);
-  nh.param<double>("thrust_cal/f4_rev", f4_rev, 1.0);
-  nh.param<double>("thrust_cal/u1_fwd", u1_fwd, 1.0);
-  nh.param<double>("thrust_cal/u1_rev", u1_rev, 1.0);
-  nh.param<double>("thrust_cal/u2_fwd", u2_fwd, 1.0);
-  nh.param<double>("thrust_cal/u2_rev", u2_rev, 1.0);
-  nh.param<double>("thrust_cal/u3_fwd", u3_fwd, 1.0);
-  nh.param<double>("thrust_cal/u3_rev", u3_rev, 1.0);
-  nh.param<double>("thrust_cal/u4_fwd", u4_fwd, 1.0);
-  nh.param<double>("thrust_cal/u4_rev", u4_rev, 1.0);
-  nh.param<double>("thrust_cal/s1_fwd", s1_fwd, 1.0);
-  nh.param<double>("thrust_cal/s1_rev", s1_rev, 1.0);
-  nh.param<double>("thrust_cal/s2_fwd", s2_fwd, 1.0);
-  nh.param<double>("thrust_cal/s2_rev", s2_rev, 1.0);
+  nh.param<double>("calibration/thruster/surge_port_hi_fw", s_p_h_fw, 1.0);
+  nh.param<double>("calibration/thruster/surge_port_hi_rv", s_p_h_rv, 1.0);
+  nh.param<double>("calibration/thruster/surge_stbd_hi_fw", s_s_h_fw, 1.0);
+  nh.param<double>("calibration/thruster/surge_stbd_hi_rv", s_s_h_rv, 1.0);
+  nh.param<double>("calibration/thruster/surge_port_lo_fw", s_p_l_fw, 1.0);
+  nh.param<double>("calibration/thruster/surge_port_lo_rv", s_p_l_rv, 1.0);
+  nh.param<double>("calibration/thruster/surge_stbd_lo_fw", s_s_l_fw, 1.0);
+  nh.param<double>("calibration/thruster/surge_stbd_lo_rv", s_s_l_rv, 1.0);
+  nh.param<double>("calibration/thruster/sway_fwd_fw", swa_f_fw, 1.0);
+  nh.param<double>("calibration/thruster/sway_fwd_rv", swa_f_rv, 1.0);
+  nh.param<double>("calibration/thruster/sway_aft_fw", swa_a_fw, 1.0);
+  nh.param<double>("calibration/thruster/sway_aft_rv", swa_a_rv, 1.0);
+  nh.param<double>("calibration/thruster/heave_port_fwd_fw", h_p_f_fw, 1.0);
+  nh.param<double>("calibration/thruster/heave_port_fwd_rv", h_p_f_rv, 1.0);
+  nh.param<double>("calibration/thruster/heave_stbd_fwd_fw", h_s_f_fw, 1.0);
+  nh.param<double>("calibration/thruster/heave_stbd_fwd_rv", h_s_f_rv, 1.0);
+  nh.param<double>("calibration/thruster/heave_port_aft_fw", h_p_a_fw, 1.0);
+  nh.param<double>("calibration/thruster/heave_port_aft_rv", h_p_a_rv, 1.0);
+  nh.param<double>("calibration/thruster/heave_stbd_aft_fw", h_s_a_fw, 1.0);
+  nh.param<double>("calibration/thruster/heave_stbd_aft_rv", h_s_a_rv, 1.0);
 }
 
-void ThrustCal::callback(const riptide_msgs::ThrustStamped::ConstPtr& force)
+void ThrustCal::callback(const riptide_msgs::ThrustStamped::ConstPtr& thrust)
 {
-  duration.header.stamp = force->header.stamp;
+  us.header.stamp = thrust->header.stamp;
 
-  duration.pwm.f1 = calibrate(force->thrust.f1, f1_fwd, f1_rev);
-  duration.pwm.f2 = calibrate(force->thrust.f2, f2_fwd, f2_rev);
-  duration.pwm.f3 = calibrate(force->thrust.f3, f3_fwd, f3_rev);
-  duration.pwm.f4 = calibrate(force->thrust.f4, f4_fwd, f4_rev);
-  duration.pwm.u1 = calibrate(force->thrust.u1, u1_fwd, u1_rev);
-  duration.pwm.u2 = calibrate(force->thrust.u2, u2_fwd, u2_rev);
-  duration.pwm.u3 = calibrate(force->thrust.u3, u3_fwd, u3_rev);
-  duration.pwm.u4 = calibrate(force->thrust.u4, u4_fwd, u4_rev);
-  duration.pwm.s1 = calibrate(force->thrust.s1, s1_fwd, s1_rev);
-  duration.pwm.s2 = calibrate(force->thrust.s2, s2_fwd, s2_rev);
+  us.pwm.surge_port_hi = cal(thrust->force.surge_port_hi, s_p_h_fw, s_p_h_rv);
+  us.pwm.surge_stbd_hi = cal(thrust->force.surge_stbd_hi, s_s_h_fw, s_s_h_rv);
+  us.pwm.surge_port_lo = cal(thrust->force.surge_port_lo, s_p_l_fw, s_p_l_rv);
+  us.pwm.surge_stbd_lo = cal(thrust->force.surge_stbd_lo, s_s_l_fw, s_s_l_rv);
+  us.pwm.sway_fwd = cal(thrust->force.sway_fwd, swa_f_fw, swa_f_rv);
+  us.pwm.sway_aft = cal(thrust->force.sway_aft, swa_a_fw, swa_a_rv);
+  us.pwm.heave_port_fwd = cal(thrust->force.heave_port_fwd, h_p_f_fw, h_p_f_rv);
+  us.pwm.heave_stbd_fwd = cal(thrust->force.heave_stbd_fwd, h_s_f_fw, h_s_f_rv);
+  us.pwm.heave_port_aft = cal(thrust->force.heave_port_aft, h_p_a_fw, h_p_a_rv);
+  us.pwm.heave_stbd_aft = cal(thrust->force.heave_stbd_aft, h_s_a_fw, h_s_a_rv);
 
-  pwm.publish(duration);
+  pwm.publish(us);
 }
 
 void ThrustCal::loop()
@@ -84,15 +84,13 @@ void ThrustCal::loop()
   ros::spin();
 }
 
-int ThrustCal::calibrate(double raw_force, double fwd_cal, double rev_cal)
+int ThrustCal::cal(double raw_force, double fwd_scale, double rev_scale)
 {
   if(raw_force < 0.0)
-  {
-    raw_force /= rev_cal;
-  }
+    raw_force /= rev_scale;
   else
-  {
-    raw_force /= fwd_cal;
-  }
-  return 1500 + int(raw_force*7.673-raw_force*raw_force*.0616-raw_force*raw_force*raw_force*.4352);
+    raw_force /= fwd_scale;
+
+  return 1500 + int(raw_force * 7.673 - raw_force * raw_force * 0.0616
+     - raw_force * raw_force * raw_force * 0.4352);
 }
