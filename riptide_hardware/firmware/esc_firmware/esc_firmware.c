47eeb4692a252b723e0eb0e6e3b898c5cdfa367e
charles fletcher <fletcher.236@osu.edu>
2016-06-18 23:41:22 -0400

    internal oscillator calibration values, offset initial clock values

--- riptide_hardware/firmware/esc_firmware/esc_firmware.c
+++ riptide_hardware/firmware/esc_firmware/esc_firmware.c
@@ -4,25 +4,42 @@
 
 void init_pwm() {
   //set fast pwm mode
-  DDRA = 0b01100000;
-  PUEA = 0b00000000;
+  DDRA = 0b01100000;//set pins as output
+  PUEA = 0b00000000;//disable internal pull up
 
-  TCCR1A = 0b00000010;
-  TCCR1B = 0b00011001;
-  ICR1H = 0b1001110;
+  TCCR1A = 0b00000010;//control register
+  TCCR1B = 0b00011010;//control register
 
+  ICR1H = 0b01001110;//TOP limit for counter
   ICR1L = 0b00100000;
-  TIMSK = 0b11100000;
+
+  TIMSK = 0b11100000;//enables interrupts
+
 
   OCR1AH = 0b101;
-  OCR1AL = 0b11011100;
+  OCR1AL = 0b11011010;
   OCR1BH = 0b101;
-  OCR1BL = 0b11011100;
+  OCR1BL = 0b11011110;
 }
 
 void main()
 {
-  unsigned char I2C_address = 0b1000;
+  //use the watchdog timer
+
+  //calibrate the internal ocillilator
+  OSCCAL0 = 65;
+    //addr #1    66
+    //addr #2    66
+    //addr #4    93
+    //addr #8    46
+    //addr #16   65
+  //change the system clock prescaler
+  CCP = 0xD8;
+  CLKPR = 0b00000000;
+
+
+
+  unsigned char I2C_address = 0b100000;
   init_pwm();
   TWI_Slave_Initialise(I2C_address);
 
@@ -48,23 +65,28 @@ void main()
 ISR(TIM1_COMPA_vect)
 {
   //turn PWM pin 1 off
-  PORTA &= 0b10111111;
+  PORTA &= 0b11011111;
 
-  OCR1AH = TWI_buf[0];
-  OCR1AL = TWI_buf[1];
+
 
 }
 
 ISR(TIM1_COMPB_vect)
 {
   //turn PWM pin 0 off
-  PORTA &= 0b11011111;
-  OCR1BH = TWI_buf[2];
-  OCR1BL = TWI_buf[3];
+  PORTA &= 0b10111111;
+
 }
 
 ISR(TIM1_OVF_vect)
 {
   //turn both pins on
   PORTA |= 0b01100000;
+
+  OCR1AH = TWI_buf[0];
+  OCR1AL = TWI_buf[1];
+  OCR1BH = TWI_buf[2];
+  OCR1BL = TWI_buf[3];
+  TWI_buf[4] = (TWI_buf[0]^TWI_buf[2])^(TWI_buf[1]^TWI_buf[3]);
+
 }
