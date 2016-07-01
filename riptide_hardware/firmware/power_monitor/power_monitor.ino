074ac640225102db97a30bfdee6d8abe038d1517
Conor Cary <cary.42@osu.edu>
2016-07-01 15:59:47 -0400

    Update power monitor rate.

--- riptide_hardware/firmware/power_monitor/power_monitor.ino
+++ riptide_hardware/firmware/power_monitor/power_monitor.ino
@@ -6,29 +6,23 @@ riptide_msgs::BatStamped battery;
 
 ros::Publisher state_pub("state/batteries", &battery);
 
-int DELAY = 200;
-int timer;
-
 void setup()
 {
   nh.initNode();
   nh.advertise(state_pub);
-  
-  timer = millis() + DELAY;
 }
 
 void loop()
 {
   nh.spinOnce();
 
-  if(timer > millis())
-  {
-    battery.port.current = (analogRead(A3)-104)*0.03755;
-    battery.port.voltage = analogRead(A2)*0.03015;
-    battery.stbd.current = (analogRead(A0)-104)*0.03755;
-    battery.stbd.voltage = analogRead(A1)*0.03016;
-  
-    state_pub.publish(&battery);
-    timer = millis() + DELAY;
-  }
+  battery.port.current = (analogRead(A3) - 104) * 0.03755;
+  battery.port.voltage = analogRead(A2) * 0.03015;
+  battery.stbd.current = (analogRead(A0) - 104) * 0.03755;
+  battery.stbd.voltage = analogRead(A1) * 0.03016;
+
+  state_pub.publish(&battery);
+
+  delay(200);
 }
+
