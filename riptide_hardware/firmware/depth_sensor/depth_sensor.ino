980024841f29f98ed2cd179e0b17ee22f71fc3b4
charles fletcher <fletcher.236@osu.edu>
2016-07-01 21:12:45 -0400

    depth sensor

---
+++ riptide_hardware/firmware/depth_sensor/depth_sensor.ino
@@ -0,0 +1,34 @@
+#include <ros.h>
+#include <riptide_msgs/Depth.h>
+#include "MS5837.h"
+
+ros::NodeHandle nh;
+riptide_msgs::Depth battery;
+
+ros::Publisher state_pub("state/depth", &depth);
+
+void setup()
+{
+  nh.initNode();
+  nh.advertise(state_pub);
+
+  Wire.begin();
+
+ sensor.init();
+
+ sensor.setFluidDensity(997); // kg/m^3 (997 freshwater, 1029 for seawater)
+}
+
+void loop()
+{
+  nh.spinOnce();
+
+  depth.depth = sensor.depth();
+  depth.temp = sensor.temperature();
+  depth.pressure = sensor.pressure();
+  depth.altitude = sensor.altitude();
+
+  state_pub.publish(&battery);
+
+  delay(40);
+}
