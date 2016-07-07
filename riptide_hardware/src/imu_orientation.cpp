fe57f83f7918299e47284c35012a34a99334de31
Conor Cary <cary.42@osu.edu>
2016-07-07 00:20:12 -0400

    Combine imu messages.

--- riptide_hardware/src/imu_orientation.cpp
+++ riptide_hardware/src/imu_orientation.cpp
@@ -1,31 +1,44 @@
 
 #include "ros/ros.h"
-#include "tf/transform_datatypes.h"
-#include "tf/LinearMath/Matrix3x3.h"
+#include "message_filters/subscriber.h"
+#include "message_filters/synchronizer.h"
+#include "message_filters/sync_policies/approximate_time.h"
+#include "sensor_msgs/Imu.h"
 #include "imu_3dm_gx4/FilterOutput.h"
-#include "geometry_msgs/Vector3Stamped.h"
 
-ros::Publisher rpy;
-ros::Subscriber imu;
-geometry_msgs::Vector3Stamped angles;
+ros::Publisher unifier;
+sensor_msgs::Imu state;
 
-void callback(const imu_3dm_gx4::FilterOutput::ConstPtr& filter)
+void callback(const sensor_msgs::Imu::ConstPtr& imu, const imu_3dm_gx4::FilterOutput::ConstPtr& filter)
 {
-  tf::Quaternion quaternion;
-  tf::quaternionMsgToTF(filter->orientation, quaternion);
-  tf::Matrix3x3 rotation = tf::Matrix3x3(1, 0, 0, 0, 1, 0, 0, 0, 1);
-  tf::Matrix3x3 attitude = rotation * tf::Matrix3x3(quaternion);
-  attitude.getRPY(angles.vector.x, angles.vector.y, angles.vector.z);
-  angles.header.stamp = filter->header.stamp;
-  rpy.publish(angles);
+ state.header = imu->header;
+
+ state.orientation = filter->orientation;
+ state.orientation_covariance = filter->orientation_covariance;
+
+ state.angular_velocity = imu->angular_velocity;
+ state.angular_velocity_covariance = imu->angular_velocity_covariance;
+
+ state.linear_acceleration = imu->linear_acceleration;
+ state.linear_acceleration_covariance = imu->linear_acceleration_covariance;
+
+ unifier.publish(state);
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "orientation");
+  ros::init(argc, argv, "imu_state");
+
   ros::NodeHandle nh;
-  imu = nh.subscribe<imu_3dm_gx4::FilterOutput>("state/filter", 1, callback);
-  rpy = nh.advertise<geometry_msgs::Vector3Stamped>("state/rpy", 1);
-  angles.header.frame_id = "base_link";
+
+  message_filters::Subscriber<sensor_msgs::Imu> imu_sub(nh, "imu/imu", 1);
+  message_filters::Subscriber<imu_3dm_gx4::FilterOutput> filter_sub(nh, "imu/filter", 1);
+
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Imu, imu_3dm_gx4::FilterOutput> approx;
+  message_filters::Synchronizer<approx> sync(approx(10), imu_sub, filter_sub);
+  sync.registerCallback(boost::bind(&callback, _1, _2));
+
+  unifier = nh.advertise<sensor_msgs::Imu>("state/imu", 1);
+
   ros::spin();
 }
