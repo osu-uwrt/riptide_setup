31d0f7e3d043433c1ab60983b689f604daf4b2df
Conor Cary <cary.42@osu.edu>
2016-09-23 19:22:49 -0400

    Lint riptide_hardware

--- riptide_hardware/src/thrust_cal.cpp
+++ riptide_hardware/src/thrust_cal.cpp
@@ -1,32 +1,60 @@
+/*********************************************************************************
+ *  Copyright (c) 2015, The Underwater Robotics Team
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************************/
+
 #include "ros/ros.h"
-#include "riptide_msgs/ThrustStamped.h"
-#include "riptide_msgs/PwmStamped.h"
-#include "riptide_msgs/Bat.h"
 #include "std_msgs/Empty.h"
+
+#include "riptide_msgs/Bat.h"
+#include "riptide_msgs/PwmStamped.h"
+#include "riptide_msgs/ThrustStamped.h"
 
 class ThrustCal
 {
-  private:
-    ros::NodeHandle nh;
-    ros::Subscriber kill_it_with_fire;
-    ros::Subscriber thrust, outta_juice;
-    ros::Publisher pwm;
-    riptide_msgs::PwmStamped us;
-    ros::Time alive;
-    bool dead, low;
-    double min_voltage;
-    int counterclockwise(double raw_force);
-    int clockwise(double raw_force);
+ private:
+  ros::NodeHandle nh;
+  ros::Subscriber kill_it_with_fire;
+  ros::Subscriber thrust, outta_juice;
+  ros::Publisher pwm;
+  riptide_msgs::PwmStamped us;
+  ros::Time alive;
+  bool dead, low;
+  double min_voltage;
+  int counterclockwise(double raw_force);
+  int clockwise(double raw_force);
 
-  public:
-    ThrustCal();
-    void callback(const riptide_msgs::ThrustStamped::ConstPtr& thrust);
-    void killback(const std_msgs::Empty::ConstPtr& thrust);
-    void voltsbacken(const riptide_msgs::Bat::ConstPtr& bat_stat);
-    void loop();
+ public:
+  ThrustCal();
+  void callback(const riptide_msgs::ThrustStamped::ConstPtr& thrust);
+  void killback(const std_msgs::Empty::ConstPtr& thrust);
+  void voltsbacken(const riptide_msgs::Bat::ConstPtr& bat_stat);
+  void loop();
 };
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
   ros::init(argc, argv, "thruster_calibration");
   ThrustCal thrust_cal;
@@ -67,7 +95,7 @@ void ThrustCal::callback(const riptide_msgs::ThrustStamped::ConstPtr& thrust)
 
 void ThrustCal::killback(const std_msgs::Empty::ConstPtr& thrust)
 {
-  if(!low)
+  if (!low)
   {
     dead = false;
   }
@@ -76,7 +104,7 @@ void ThrustCal::killback(const std_msgs::Empty::ConstPtr& thrust)
 
 void ThrustCal::voltsbacken(const riptide_msgs::Bat::ConstPtr& bat_stat)
 {
-  if(bat_stat->voltage < min_voltage)
+  if (bat_stat->voltage < min_voltage)
   {
     low = true;
   }
@@ -86,10 +114,10 @@ void ThrustCal::loop()
 {
   ros::Duration safe(0.05);
   ros::Rate rate(50);
-  while(!ros::isShuttingDown())
+  while (!ros::isShuttingDown())
   {
     ros::Duration timeout = ros::Time::now() - alive;
-    if(timeout > safe)
+    if (timeout > safe)
     {
       dead = true;
     }
@@ -101,9 +129,9 @@ void ThrustCal::loop()
 int ThrustCal::counterclockwise(double raw_force)
 {
   int pwm = 1500;
-  if(!dead)
+  if (!dead)
   {
-    pwm = 1500 + int(raw_force * 14);
+    pwm = 1500 + static_cast<int>(raw_force * 14);
   }
   return pwm;
 }
@@ -112,9 +140,9 @@ int ThrustCal::clockwise(double raw_force)
 {
   int pwm = 1500;
 
-  if(!dead)
+  if (!dead)
   {
-    pwm = 1500 - int(raw_force * 14);
+    pwm = 1500 - static_cast<int>(raw_force * 14);
   }
 
   return pwm;
