c9c53bcd8ab4603b87ceefb76159fd67dad9a898
tsender <sender.9@osu.edu>
2017-10-05 19:59:48 -0400

    Added message callback

--- riptide_hardware/include/riptide_hardware/imu_processor.h
+++ riptide_hardware/include/riptide_hardware/imu_processor.h
@@ -41,16 +41,19 @@ class IMUProcessor
 {
 private:
   ros::NodeHandle nh;
-
   ros::Subscriber imu_filter_sub;
   ros::Publisher imu_state_pub;
 
   //0= current state, 1 = one state ago, 2 = two states ago
   riptide_msgs::Imu state[3];
-public:
-  IMUProcessor(char **argv, const riptide_msgs::Imu::ConstPtr& old_filt_output);
-  void callback(const imu_3dm_gx4::FilterOutput::ConstPtr& old_filt_output);
 
+  //Change in time between the three states
+  //dt[0] = time b/w state[0] and state[1], dt[1] = time b/w state[1] and state[2]
+  float dt[2];
+public:
+  IMUProcessor(char **argv);
+  void callback(const imu_3dm_gx4::FilterOutput::ConstPtr& filter_msg);
+  void loop();
 };
 
 #endif
--- riptide_hardware/src/imu_processor.cpp
+++ riptide_hardware/src/imu_processor.cpp
@@ -41,19 +41,42 @@
    imu_state_pub = nh.advertise<riptide_msgs::Imu>("state/imu", 1);
  }
 
+ //Callback
+  void IMUProcessor::callback(const imu_3dm_gx4::FilterOutput::ConstPtr& filter_msg)
+  {
+    //Take in message into state[0]
+    state[0].header = filter_msg->header;
+    state[0].header.frame_id = "base_link";
+    state[0].euler_rpy = filter_msg->euler_rpy;
+    state[0].euler_rpy_status = filter_msg->euler_rpy_status;
+    state[0].linear_acceleration = filter_msg->linear_acceleration;
+    state[0].linear_acceleration_status = filter_msg->linear_acceleration_status;
+    state[0].angular_velocity = filter_msg->angular_velocity;
+    state[0].angular_velocity_status = filter_msg->angular_velocity_status;
+
+    //Set new delta time (nano seconds)
+    if(dt[1] != 0) {
+      dt[0] = state[0].header.stamp.nsec - dt[1];
+    }
+
+    //Process linear acceleration (Remove centrifugal and tangential components)
+
+    //Process angular acceleration (Use 3-pt backwards rule to approximate angular acceleration)
+
+    //Publish message for current state, adjust states and dt arrays
+    imu_state_pub.publish(state[0]);
+    state[2] = state[1];
+    state[1] = state[0];
+    dt[1] = dt[0]; //Will set dt[0] upon receiving new message
+  }
+
 //ROS loop function
  void IMUProcessor::loop()
  {
-   //ros::Rate rate(50);
+   ros::Rate rate(1000);
    while (!ros::isShuttingDown())
    {
      ros::spinOnce();
      rate.sleep();
    }
- }
-
-//Callback
- void IMUProcessor::callback(const imu_3dm_gx4::FilterOutput::ConstPtr& filter_msg)
- {
-
  }
