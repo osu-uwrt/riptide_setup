71883e817bcf580a3fb8721cf343b5e5c43a130a
tsender <sender.9@osu.edu>
2017-10-08 21:25:54 -0400

    Created uwrt.rules file, adjusted imu.launch file accordingly, and updated imu_processor code

--- riptide_hardware/include/riptide_hardware/imu_processor.h
+++ riptide_hardware/include/riptide_hardware/imu_processor.h
@@ -36,6 +36,7 @@
 #include "riptide_msgs/Imu.h"
 #include "std_msgs/Header.h"
 #include "imu_3dm_gx4/FilterOutput.h"
+#include "math.h"
 
 class IMUProcessor
 {
@@ -44,12 +45,9 @@ private:
   ros::Subscriber imu_filter_sub;
   ros::Publisher imu_state_pub;
 
-  //0= current state, 1 = one state ago, 2 = two states ago
+  //0 = current state, 1 = one state ago, 2 = two states ago
   riptide_msgs::Imu state[3];
-
-  //Change in time between the three states
-  //dt[0] = time b/w state[0] and state[1], dt[1] = time b/w state[1] and state[2]
-  float dt[2];
+  float zero_ang_vel_thresh;
 public:
   IMUProcessor(char **argv);
   void callback(const imu_3dm_gx4::FilterOutput::ConstPtr& filter_msg);
--- riptide_hardware/src/imu_processor.cpp
+++ riptide_hardware/src/imu_processor.cpp
@@ -27,6 +27,8 @@
 
  #include "riptide_hardware/imu_processor.h"
 
+ #define PI 3.141592653
+
  int main(int argc, char** argv)
  {
    ros::init(argc, argv, "imu_processor");
@@ -39,14 +41,17 @@
  {
    imu_filter_sub = nh.subscribe<imu_3dm_gx4::FilterOutput>("imu/filter", 1, &IMUProcessor::callback, this);
    imu_state_pub = nh.advertise<riptide_msgs::Imu>("state/imu", 1);
+
+   zero_ang_vel_thresh = 1;
  }
 
  //Callback
   void IMUProcessor::callback(const imu_3dm_gx4::FilterOutput::ConstPtr& filter_msg)
   {
-    //Take in message into state[0]
+    //Put message data into state[0]
     state[0].header = filter_msg->header;
     state[0].header.frame_id = "base_link";
+    state[0].raw_euler_rpy = filter_msg->euler_rpy;
     state[0].euler_rpy = filter_msg->euler_rpy;
     state[0].euler_rpy_status = filter_msg->euler_rpy_status;
     state[0].linear_acceleration = filter_msg->linear_acceleration;
@@ -54,20 +59,84 @@
     state[0].angular_velocity = filter_msg->angular_velocity;
     state[0].angular_velocity_status = filter_msg->angular_velocity_status;
 
-    //Set new delta time (nano seconds)
-    if(dt[1] != 0) {
-      dt[0] = state[0].header.stamp.nsec - dt[1];
+    //Convert angular values from radians to degrees
+    state[0].raw_euler_rpy.x *= (180/PI);
+    state[0].raw_euler_rpy.y *= (180/PI);
+    state[0].raw_euler_rpy.z *= (180/PI);
+    state[0].euler_rpy.x *= (180/PI);
+    state[0].euler_rpy.y *= (180/PI);
+    state[0].euler_rpy.z *= (180/PI);
+    state[0].angular_velocity.x *= (180/PI);
+    state[0].angular_velocity.y *= (180/PI);
+    state[0].angular_velocity.z *= (180/PI);
+
+    //Adjust direction/sign of Euler Angles to be consistent with AUV's axes
+    if(state[0].euler_rpy.x > -180 && state[0].euler_rpy.x < 0) {
+      state[0].euler_rpy.x += 180;
+      state[0].raw_euler_rpy.x += 180;
     }
+    else if(state[0].euler_rpy.x > 0 && state[0].euler_rpy.x < 180) {
+      state[0].euler_rpy.x -= 180;
+      state[0].raw_euler_rpy.x -= 180;
+    }
+    else if(state[0].euler_rpy.x == 0) {
+      state[0].euler_rpy.x = 180;
+      state[0].raw_euler_rpy.x = 180;
+    }
+    else if(state[0].euler_rpy.x == 180 || state[0].euler_rpy.x == -180) {
+      state[0].euler_rpy.x = 0;
+      state[0].raw_euler_rpy.x = 0;
+    }
+    state[0].euler_rpy.z *= -1; //Negate yaw angle
+    state[0].raw_euler_rpy.z *= -1;
+
+    //Set new delta time (convert nano seconds to seconds)
+    if(state[1].dt == 0) { //If dt[0] not set yet, set to time in current header stamp
+      state[0].dt = (double)state[0].header.stamp.sec + state[0].header.stamp.nsec/(1.0e9);
+    }
+    else { //dt has already been set, so find actual dt
+      state[0].dt = (double)state[0].header.stamp.sec + state[0].header.stamp.nsec/(1.0e9) - state[1].dt;
+    }
+
+    //Process Drift
+    //Check if angular velocity about any axis is approximately 0
+    if(abs(state[0].angular_velocity.x) <= zero_ang_vel_thresh) {
+      state[0].local_drift.x = state[0].euler_rpy.x - state[1].euler_rpy.x;
+    }
+    else {
+      state[0].local_drift.x = 0;
+    }
+    state[0].cumulative_drift.x = abs(state[1].cumulative_drift.x) + abs(state[0].local_drift.x);
+    state[0].local_drift_rate.x = state[0].local_drift.x / state[0].dt;
+
+    if(abs(state[0].angular_velocity.y) <= zero_ang_vel_thresh) {
+      state[0].local_drift.y = state[0].euler_rpy.y - state[1].euler_rpy.y;
+    }
+    else {
+      state[0].local_drift.y = 0;
+    }
+    state[0].cumulative_drift.y += abs(state[1].cumulative_drift.y) + abs(state[0].local_drift.y);
+    state[0].local_drift_rate.y = state[0].local_drift.y / state[0].dt;
+
+    if(abs(state[0].angular_velocity.z) <= zero_ang_vel_thresh) {
+      state[0].local_drift.z = state[0].euler_rpy.z - state[1].euler_rpy.z;
+    }
+    else {
+      state[0].local_drift.z = 0;
+    }
+    state[0].cumulative_drift.z += abs(state[1].cumulative_drift.z) + abs(state[0].local_drift.z);
+    state[0].local_drift_rate.z = state[0].local_drift.z / state[0].dt;
 
     //Process linear acceleration (Remove centrifugal and tangential components)
 
     //Process angular acceleration (Use 3-pt backwards rule to approximate angular acceleration)
 
     //Publish message for current state, adjust states and dt arrays
-    imu_state_pub.publish(state[0]);
+    //imu_state_pub.publish(state[0]);
     state[2] = state[1];
     state[1] = state[0];
-    dt[1] = dt[0]; //Will set dt[0] upon receiving new message
+    imu_state_pub.publish(state[1]);
+    //dt = dt; //Will set dt[0] upon receiving new message
   }
 
 //ROS loop function
