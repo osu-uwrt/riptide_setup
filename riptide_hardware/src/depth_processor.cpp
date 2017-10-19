1fa76a34997d98550a34d55f5268de8fc14b5f45
Benji Justice <justbenj97@gmail.com>
2017-10-19 18:42:31 -0400

    Added depth processor

--- riptide_hardware/src/depth_processor.cpp
+++ riptide_hardware/src/depth_processor.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
 }
 
 //Constructor
-DepthProcessor::DepthProcessor()
+DepthProcessor::DepthProcessor() : nh()
 {
  depth_sub = nh.subscribe<riptide_msgs::Depth>("arduino/depth", 1, &DepthProcessor::DepthCB, this);
  state_depth_pub = nh.advertise<riptide_msgs::Depth>("state/depth", 1);
@@ -17,7 +17,6 @@ DepthProcessor::DepthProcessor()
 //Callback
 void DepthProcessor::DepthCB(const riptide_msgs::Depth::ConstPtr& depth)
 {
-    // Adjust Depth according to calibration
     riptide_msgs::Depth corrected;
     corrected.depth = (DEPTH_SLOPE * depth->depth) + DEPTH_OFFSET;
     state_depth_pub.publish(corrected);
