8ef77c03ebd909f484e2168ead1c50ee14b820e6
tsender <sender.9@osu.edu>
2018-02-25 17:31:13 -0500

    Began writing alignment server in c++

--- riptide_autonomy/scripts/servers/alignment_server.cpp
+++ riptide_autonomy/scripts/servers/alignment_server.cpp
@@ -8,4 +8,7 @@ typedef actionlib::SimpleActionServer<riptide_autonomy::AlignmentAction> Server;
 class AlignmentActionServer {
 private:
   ros::NodeHandle nh;
+
+public:
+  void executeCB(const riptide_autonomy::AlignmentAction )
 }
