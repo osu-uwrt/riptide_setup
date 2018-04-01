267d8156a412f1c6749beeffd1fda08a76704c16
tsender <sender.9@osu.edu>
2018-04-01 12:40:42 -0400

    Fixed mag_offset_calibration.py and added copro to riptide.rules

--- riptide_hardware/scripts/mag_offset_calibration.py
+++ riptide_hardware/scripts/mag_offset_calibration.py
@@ -2,6 +2,7 @@
 
 import rospy
 import csv
+import sys
 from riptide_msgs.msg import PwmStamped
 from imu_3dm_gx4.msg import MagFieldCF
 
@@ -12,64 +13,107 @@ magTotal = 0
 pwm_msg = PwmStamped()
 
 def magCallback(data):
+    global magX, magY, magZ, magTotal
     magX = data.mag_field_components.x
     magY = data.mag_field_components.y
     magZ = data.mag_field_components.z
     magTotal = data.mag_field_magnitude
 
 def pwmFillMsg(pwm):
-    pwm_msg.header = rospy.Time.now()
+    pwm_msg.header.stamp = rospy.Time.now()
     pwm_msg.pwm.surge_port_hi = 1500
     pwm_msg.pwm.surge_stbd_hi = 1500
     pwm_msg.pwm.surge_port_lo = pwm
     pwm_msg.pwm.surge_stbd_lo = 1500
     pwm_msg.pwm.sway_fwd = 1500
-    pwm_msg.pwm.sway_fwd = 1500
+    pwm_msg.pwm.sway_aft = 1500
     pwm_msg.pwm.heave_port_fwd = 1500
     pwm_msg.pwm.heave_stbd_fwd = 1500
     pwm_msg.pwm.heave_port_aft = 1500
     pwm_msg.pwm.heave_stbd_aft = 1500
 
+def writeToFile(filename, pwm):
+    with open(filename, 'a+') as file:
+        file.write('%i, %f, %f, %f, %f\n' %(pwm, magX, magY, magZ, magTotal))
+        print("Wrote values to file")
+        print(pwm)
+    file.close()
+
 def main():
     rospy.init_node('mag_offset_calibration')
-    pwmPub = rospy.Publisher('command/pwm', PwmStamped)
-    magSub = rospy.Subscriber("/imu/imu_magnetic_field", MagFieldCF, magCallback)
+    pwmPub = rospy.Publisher('command/pwm', PwmStamped, queue_size=10)
+    magSub = rospy.Subscriber("/imu/magnetic_field", MagFieldCF, magCallback)
 
     rate = rospy.Rate(100)
-    done = False
+    state = 'pubPWM'
     pwm = 1500
     increment = 25
     low = 1300
     high = 1700
+    filename = "/home/tsender/osu-uwrt/riptide_software/src/riptide_hardware/cfg/test.csv"
+    time = rospy.Time.now().to_sec()
+    count = 0
+    ramp = 0
 
     while not rospy.is_shutdown():
-        with open('test.csv', 'a') as csvfile:
-            testWriter = csv.writer(csvfile)
+        #file1 = open(filename, 'a+')
+        #with open(filename, 'a+'):
+        #testWriter = csv.writer(csvfile)
+        #rospy.
 
-            while done == False:
-                #Do three ramps (up, down, up)
-                for ramp in range(0,3):
-                    while pwm <= high and pwm >= low:
-                        pwmFillMsg(pwm)
-                        pwmPub.publish(pwm_msg)
-                        rospy.sleep(2.0)
+        #Do three ramps (up, down, up)
+        #print(state)
+        if state == 'pubPWM':
+            pwmFillMsg(pwm)
+            pwmPub.publish(pwm_msg)
+            if rospy.Time.now().to_sec() - time > 2.0:
+                state = 'writeValues'
+                time = rospy.Time.now().to_sec()
+        elif state == 'writeValues':
+            if count < 4 and (rospy.Time.now().to_sec() - time) >= 0.25:
+                writeToFile(filename, pwm)
+                time = rospy.Time.now().to_sec()
+                count = count + 1
+            elif count == 4:
+                state = 'setPWM'
+                count = 0
+        elif state == 'setPWM':
+            if pwm < high and pwm > low:
+                pwm = pwm + (-1)**ramp * increment
+            elif ramp == 0:
+                pwm = high - increment #Start going down
+                ramp = ramp + 1
+            elif ramp == 1:
+                pwm = low + increment #Start going back up
+                ramp = ramp + 1
+            state = 'pubPWM'
+            if ramp == 2 and pwm == 1525:
+                state = 'done'
+            time = rospy.Time.now().to_sec()
 
-                        #Get mag values 5 times
-                        for i in range(0, 5):
-                            testWriter.writerow([pwm, magX, magY, magZ, magTotal])
-                            rospy.sleep(0.5)
-
-                        #If testing is done (ramp = 2, and pwm back at 1500), break
-                        if ramp == 2 and pwm == 1500:
-                            done = True
-                            break
-
-                        #Increment or decrement pwm
-                        pwm = pwm + (-1)**ramp * increment
-                    if ramp == 0:
-                        pwm = high - increment #Start going down
-                    elif ramp == 1:
-                        pwm = low + increment #Start going back up
+        # for ramp in range(0,3):
+        #     while pwm <= high and pwm >= low:
+        #         pwmFillMsg(pwm)
+        #         pwmPub.publish(pwm_msg)
+        #         rospy.sleep(2.0)
+        #
+        #         #Get mag values 5 times
+        #         for i in range(0, 5):
+        #             writeToFile(filename, pwm)
+        #             rospy.sleep(0.5)
+        #             if rospy.is_shutdown():
+        #                 done = True
+        #
+        #         #If testing is done (ramp = 2, and pwm back at 1500), break
+        #         if ramp == 2 and pwm == 1500:
+        #             break
+        #
+        #         #Increment or decrement pwm
+        #         pwm = pwm + (-1)**ramp * increment
+        #     if ramp == 0:
+        #         pwm = high - increment #Start going down
+        #     elif ramp == 1:
+        #         pwm = low + increment #Start going back up
 
         rate.sleep()
 
