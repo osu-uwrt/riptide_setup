c9e0e1bbae59e282826de34d001ee19115044782
Benji Justice <justbenj97@gmail.com>
2018-04-12 20:45:37 -0400

    Added vision controller and config files

---
+++ riptide_controllers/include/riptide_controllers/alignment_controller.h
@@ -0,0 +1,39 @@
+#ifndef ALIGNMENT_CONTROLLER_H
+#define ALIGNMENT_CONTROLLER_H
+
+#include "ros/ros.h"
+#include "control_toolbox/pid.h"
+#include "std_msgs/Float64.h"
+#include "riptide_msgs/GateData.h"
+
+class AlignmentController
+{
+  private:
+    // Comms
+    ros::NodeHandle nh;
+    ros::Subscriber object_sub;
+
+    ros::Publisher cmd_pub;
+
+    control_toolbox::Pid y_pid;
+    std_msgs::Float64 accel;
+
+    //PID
+    double y_error;
+    double d_y_error;
+    double last_y_error;
+    double dt;
+
+    bool pid_initialized;
+
+    ros::Time sample_start;
+    ros::Duration sample_duration;
+
+    void UpdateError();
+
+  public:
+    AlignmentController();
+    void ObjectCB(const riptide_msgs::ObjectData::ConstPtr &msg);
+ };
+
+ #endif
--- riptide_vision/scripts/qualification_vision/qualification_vision.py
+++ riptide_vision/scripts/qualification_vision/qualification_vision.py
@@ -3,14 +3,19 @@
 # Subscribes to camera output, publishes what it sees.
 
 import rospy
+import cv2
+import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image, CompressedImage
 from riptide_vision import RiptideVision
+from riptide_msgs.msg import GateData
+from geometry_msgs.msg import Vector3
 
 class ImageProcessor:
     def __init__(self):
         self.image_pub = rospy.Publisher("/forward/processed/compressed", CompressedImage, queue_size=1)
-        self.fwd_sub = rospy.Subscriber("/forward/image_raw", Image, self.image_callback, queue_size=1)
+        self.data_pub = rospy.Publisher("/state/vision/gate", GateData, queue_size=1)
+        self.fwd_sub = rospy.Subscriber("/forward/image_raw/compressed", CompressedImage, self.image_callback, queue_size=1)
         self.bridge = CvBridge()
 
     def image_callback(self, data):
@@ -24,11 +29,28 @@ class ImageProcessor:
         if good:
             response = RiptideVision().detect_gate(cv_image)
             hud_img = RiptideVision().detect_gate_vis(cv_image, response)
-            msg = CompressedImage()
-            msg.header.stamp = rospy.Time.now()
-            msg.format = ".jpeg"
-            msg.data = RiptideVision().compressed_img_msg_data(".jpeg", hud_img)
-            self.image_pub.publish(msg)
+            img = CompressedImage()
+            img.header.stamp = rospy.Time.now()
+            img.format = ".jpeg"
+            img.data = RiptideVision().compressed_img_msg_data(".jpeg", hud_img)
+            self.image_pub.publish(img)
+
+            msg = GateData()
+            msg.object_data.header.stamp = rospy.Time.now()
+            msg.object_data.visible = False
+            if (len(response) > 0):
+                pos = Vector3()
+                pos.x = 0
+                pos.y = response[3]
+                pos.z = response[4]
+                msg.left_pole_visible = response[1]
+                msg.right_pole_visible = response[2]
+                msg.object_data.visible = True
+                msg.object_data.relative_position = pos
+
+            self.data_pub.publish(msg)
+
+
 def main():
     rospy.init_node('qualification_vision')
     ip = ImageProcessor()
