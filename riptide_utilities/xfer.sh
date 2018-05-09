154f8c8080d9d6c051cbdd39a6087bdc8f8e176e
tsender <sender.9@osu.edu>
2018-05-08 23:08:31 -0400

    Added individual reset for roll, pitch, and yaw PID

--- riptide_utilities/xfer.sh
+++ riptide_utilities/xfer.sh
@@ -1,3 +1,3 @@
-ssh ros@riptide 'rm -rf /home/ros/osu-uwrt/src'
-scp -r ~/osu-uwrt/src ros@riptide:/home/ros/osu-uwrt/
-ssh ros@riptide 'cd /home/ros/osu-uwrt && source /opt/ros/kinetic/setup.bash && catkin_make'
+ssh ros@riptide 'rm -rf /home/ros/osu-uwrt/riptide_software/src'
+scp -r ~/osu-uwrt/riptide_software/src ros@riptide:/home/ros/osu-uwrt/riptide_software/src
+ssh ros@riptide 'cd /home/ros/osu-uwrt/riptide_software && source /opt/ros/kinetic/setup.bash && catkin_make'
