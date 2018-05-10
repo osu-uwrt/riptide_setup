46348b53eca8397e1f61f8203021e56cf3138fbb
edinpeter <edin.3@osu.edu>
2018-05-10 14:10:00 -0400

    Added date script, ssh keys fix

--- riptide_utilities/date_set.sh
+++ riptide_utilities/date_set.sh
@@ -1,4 +1,11 @@
 date_seconds=$(date +"%s")
-echo "$date_seconds"
 date_string=$(date --date=@$date_seconds)
-sudo date --set="$date_string"
+echo "$date_string"
+ssh ros@riptide "sudo date --set=\"$date_string\""
+ssh ros@riptide "sudo hwclock --systohc"
+
+# To change robot clock / hw clock without passwords:
+#sudo visudo
+#copy this line to bottom:
+#ros ALL=(ALL) NOPASSWD: /bin/date, /sbin/hwclock
+
