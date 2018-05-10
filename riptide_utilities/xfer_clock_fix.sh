e15aef41fee2ff1cb0c0f79fd7bafa9e6abe2a7b
edinpeter <edin.3@osu.edu>
2018-05-10 12:01:21 -0400

    Added some more potential clock fixes

--- riptide_utilities/xfer_clock_fix.sh
+++ riptide_utilities/xfer_clock_fix.sh
@@ -1,4 +1,3 @@
-#ssh ros@riptide 'rm -rf /home/ros/osu-uwrt/riptide_software/src'
 rsync -tvrz ~/osu-uwrt/riptide_software/src ros@riptide:/home/ros/osu-uwrt/riptide_software
 # Time stuff:
 
@@ -13,5 +12,6 @@ ssh ros@riptide 'sudo hwclock --systohc'
 
 ssh ros@riptide 'cd /home/ros/osu-uwrt/riptide_software && source ~/osu-uwrt/riptide_software/devel/setup.bash && source /opt/ros/kinetic/setup.bash && catkin_make'
 
-
+#sudo visudo
+#copy this line to bottom:
 #ros ALL=(ALL) NOPASSWD: /bin/date, /sbin/hwclock
