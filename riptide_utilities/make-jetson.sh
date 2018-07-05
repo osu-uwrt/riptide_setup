258b84b5fc32b0d5c11ce045b0ddbf7c42fc1a66
tsender <sender.9@osu.edu>
2018-07-05 17:40:08 -0400

    New xfer and make scripts for each computer

--- riptide_utilities/make-jetson.sh
+++ riptide_utilities/make-jetson.sh
@@ -5,4 +5,7 @@ rsync -tvrz ~/osu-uwrt/riptide_software/src/darknet_ros ros@jetson:~/osu-uwrt/ri
 rsync -tvrz ~/osu-uwrt/riptide_software/src/riptide_vision ros@jetson:~/osu-uwrt/riptide_software/src
 rsync -tvrz ~/osu-uwrt/riptide_software/src/riptide_msgs ros@jetson:~/osu-uwrt/riptide_software/src
 rsync -tvrz ~/osu-uwrt/riptide_software/src/riptide_utilities ros@jetson:~/osu-uwrt/riptide_software/src
+
+#ssh ros@jetson 'cd ~/osu-uwrt/riptide_software && source /opt/ros/kinetic/setup.bash && catkin_make && source ~/osu-uwrt/riptide_software/devel/setup.bash'
+ssh ros@jetson 'cd ~/osu-uwrt/riptide_software && catkin_make'
 ssh ros@jetson 'chmod 700 ~/osu-uwrt/riptide_software/src/riptide_utilities/*'
---
+++ riptide_utilities/make-riptide.sh
@@ -0,0 +1,5 @@
+#ssh ros@riptide 'rm -rf ~/osu-uwrt/riptide_software/src'
+rsync -tvrz ~/osu-uwrt/riptide_software/src ros@riptide:~/osu-uwrt/riptide_software
+#ssh ros@riptide 'cd ~/osu-uwrt/riptide_software && source /opt/ros/kinetic/setup.bash && catkin_make && source ~/osu-uwrt/riptide_software/devel/setup.bash'
+ssh ros@riptide 'cd ~/osu-uwrt/riptide_software && catkin_make'
+ssh ros@riptide 'chmod 700 ~/osu-uwrt/riptide_software/src/riptide_utilities/*.sh'
