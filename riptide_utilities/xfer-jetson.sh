f163f59aa6a4885c80bf9fb7623733133374fe14
tsender <sender.9@osu.edu>
2018-07-07 19:44:11 -0400

    Fixed machine tags and xfer scripts. Works at runtime now

--- riptide_utilities/xfer-jetson.sh
+++ riptide_utilities/xfer-jetson.sh
@@ -1,6 +1,7 @@
 #ssh ros@riptide 'rm -rf ~/osu-uwrt/riptide_software/src'
 rsync -tvrz ~/osu-uwrt/riptide_software/src/riptide_bringup ros@jetson:~/osu-uwrt/riptide_software/src
 rsync -tvrz ~/osu-uwrt/riptide_software/src/riptide_hardware ros@jetson:~/osu-uwrt/riptide_software/src
+rsync -tvrz ~/osu-uwrt/riptide_software/src/imu_3dm_gx4 ros@jetson:~/osu-uwrt/riptide_software/src
 rsync -tvrz ~/osu-uwrt/riptide_software/src/darknet_ros ros@jetson:~/osu-uwrt/riptide_software/src
 rsync -tvrz ~/osu-uwrt/riptide_software/src/riptide_vision ros@jetson:~/osu-uwrt/riptide_software/src
 rsync -tvrz ~/osu-uwrt/riptide_software/src/riptide_msgs ros@jetson:~/osu-uwrt/riptide_software/src
