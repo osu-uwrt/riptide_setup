2996a5ff5534e3159d1c7a00c42a835fb4fa8385
tsender <sender.9@osu.edu>
2018-07-20 02:08:39 -0400

    Initial version of roulette task complete. Run time errors with switch panel fixed.

--- riptide_autonomy/include/riptide_autonomy/be_autonomous.h
+++ riptide_autonomy/include/riptide_autonomy/be_autonomous.h
@@ -10,20 +10,21 @@
 #include "riptide_msgs/TaskInfo.h"
 #include "riptide_msgs/MissionState.h"
 
-#include "sensor_msgs/image_encodings.h"
-#include "opencv2/opencv.hpp"
-#include "cv_bridge/cv_bridge.h"
-
 #include "riptide_msgs/SwitchState.h"
 #include "riptide_msgs/ResetControls.h"
 #include "riptide_msgs/PwmStamped.h"
+
 #include "riptide_msgs/Imu.h"
 #include "riptide_msgs/Depth.h"
-#include "darknet_ros_msgs/BoundingBoxes.h"
 #include "riptide_msgs/AttitudeCommand.h"
 #include "riptide_msgs/DepthCommand.h"
 #include "riptide_msgs/AlignmentCommand.h"
-#include "riptide_msgs/PwmStamped.h"
+#include "riptide_msgs/Pneumatics.h"
+
+#include "sensor_msgs/image_encodings.h"
+#include "opencv2/opencv.hpp"
+#include "cv_bridge/cv_bridge.h"
+#include "darknet_ros_msgs/BoundingBoxes.h"
 
 #include "riptide_autonomy/tslam.h"
 #include "riptide_autonomy/roulette.h"
@@ -40,28 +41,33 @@ public:
   // Subscribers, Publishers, and Timers
   ros::NodeHandle nh;
   ros::Subscriber switch_sub, imu_sub, depth_sub;
-  ros::Publisher linear_accel_pub, attitude_pub, depth_pub, alignment_pub, pwm_pub, reset_pub;
-  ros::Publisher task_info_pub, state_mission_pub;
+  ros::Publisher linear_accel_pub, attitude_pub, depth_pub, alignment_pub, pneumatics_pub;
+  ros::Publisher pwm_pub, reset_pub, task_info_pub, state_mission_pub;
   ros::Timer timer;
 
+  // Mission End/System Check
+  int thruster;
+  riptide_msgs::ResetControls reset_msg;
+  riptide_msgs::PwmStamped pwm_msg;
+  riptide_msgs::AlignmentCommand align_cmd;
+  riptide_msgs::AttitudeCommand attitude_cmd;
+
   // Mission Info
-  int execute_id, load_id, last_load_id;
-  double load_duration, loader_timer;
-  bool mission_loaded, vehicle_ready;
-  ros::Time load_time;
+  int execute_id, load_id, last_load_id, last_kill_switch_value;
+  double load_duration, loader_timer, pre_start_duration, start_timer;
+  bool mission_loaded, mission_running, clock_is_ticking;
+  ros::Time load_time, pre_start_time;
 
   // Task Info
   YAML::Node tasks, task_map;
   string task_file, task_map_file, task_name, object_name;
   double search_depth, search_accel, detection_duration_thresh;
-  int align_thresh, bbox_thresh, detections_req;
+  int num_objects, align_thresh, bbox_thresh, detections_req;
   int competition_id, quadrant, task_id, last_task_id, total_tasks, tasks_enqueued, task_order_index;
   int alignment_plane, color, frame_width, frame_height;
   vector<int> task_order;
+  vector<string> object_names;
   bool single_test;
-
-  // System Check
-  int thruster;
 
   // Vehicle State
   geometry_msgs::Vector3 euler_rpy, linear_accel;
@@ -81,9 +87,10 @@ public:
   BeAutonomous();
   template <typename T>
   void LoadParam(string param, T &var);
-  void StartTaskTimer(const ros::TimerEvent& event);
   void StartTask();
   void EndMission();
+  void SendInitMsgs();
+  void SendResetMsgs();
   void SystemCheckTimer(const ros::TimerEvent& event);
   void UpdateTaskInfo();
   void ReadMap();
@@ -91,6 +98,7 @@ public:
   void SystemCheck(const ros::TimerEvent& event);
   void EndTSlamTimer(const ros::TimerEvent& event);
   void EndTSlam();
+  void ResetSwitchPanel();
   void SwitchCB(const riptide_msgs::SwitchState::ConstPtr& switch_msg);
   void ImuCB(const riptide_msgs::Imu::ConstPtr & imu_msg);
   void DepthCB(const riptide_msgs::Depth::ConstPtr& depth_msg);
