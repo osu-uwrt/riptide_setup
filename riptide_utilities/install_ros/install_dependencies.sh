51d951e5155279d0dd9ce98a788a4b97efbec8e5
tsender <sender.9@osu.edu>
2018-08-26 18:15:18 -0400

    Cleaned up installation scripts

--- riptide_utilities/install_ros/install_dependencies.sh
+++ riptide_utilities/install_ros/install_dependencies.sh
@@ -1,9 +1,10 @@
-# ROS Dependencies
+# UWRT Dependencies
+# Install ROS-related dependencies
 sudo apt-get -y install ros-kinetic-pointgrey-camera-driver 
 sudo apt-get -y install ros-kinetic-control-toolbox
 sudo apt-get -y install ros-kinetic-joy
 
-# Ceres install
+# Install Ceres
 sudo apt-get -y install cmake
 sudo apt-get -y install libgoogle-glog-dev
 sudo apt-get -y install libatlas-base-dev
@@ -19,3 +20,13 @@ cmake ../ceres-solver-1.14.0
 make -j3
 sudo make install
 rm -rf ~/ceres-temp
+
+# Setup Acoustics
+tar -zxf ~/osu-uwrt/riptide_software/src/riptide_hardware/resources/acoustics/FrontPanel-Ubuntu16.04LTS-x64-5.0.1.tgz
+cd ~/FrontPanel-Ubuntu16.04LTS-x64-5.0.1
+chmod +x install.sh
+sudo install.sh
+sudo cp API/libokFrontPanel.so /usr/lib/
+rm -rf ~/FrontPanel-Ubuntu16.04LTS-x64-5.0.1
+
+echo "Completed installation of UWRT dependencies\n"
--- riptide_utilities/install_ros/setup_uwrt_env.sh
+++ riptide_utilities/install_ros/setup_uwrt_env.sh
@@ -1,6 +1,9 @@
 # Setup UWRT Environment
+# Install ROS and dependencies
 ~/osu-uwrt/riptide_software/src/riptide_utilities/install_ros/install_ros_kinetic.sh
 ~/osu-uwrt/riptide_software/src/riptide_utilities/install_ros/install_dependencies.sh
+
+# Setup ~/.bashrc file
 echo "source ~/osu-uwrt/sim_software/devel/setup.bash" >> ~/.bashrc
 echo "source ~/osu-uwrt/shared_software/devel/setup.bash" >> ~/.bashrc
 echo "source ~/osu-uwrt/riptide_software/devel/setup.bash" >> ~/.bashrc
@@ -9,5 +12,6 @@ echo "#export ROS_MASTER_URI=http://jetosn:11311" >> ~/.bashrc
 echo "export ROS_MASTER_URI=http://$(hostname):11311" >> ~/.bashrc
 source ~/.bashrc
 
+# Add user to group 'uwrt' for sensor permissions
 sudo ~/osu-uwrt/riptide_software/src/riptide_hardware/scripts/add_rule
-echo "Please reboot your computer for final changes to take effect"
+echo "Please reboot your computer for final changes to take effect\n"
