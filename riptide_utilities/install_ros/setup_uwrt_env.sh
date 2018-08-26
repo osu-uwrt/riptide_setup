44dd5ca7996ebda1d907a8b2476afcd804158849
tsender <sender.9@osu.edu>
2018-08-26 10:31:42 -0400

    More revisions to UWRT setup

--- riptide_utilities/install_ros/setup_uwrt_env.sh
+++ riptide_utilities/install_ros/setup_uwrt_env.sh
@@ -7,6 +7,7 @@ echo "source ~/osu-uwrt/riptide_software/devel/setup.bash" >> ~/.bashrc
 echo "#export ROS_MASTER_URI=http://riptide:11311" >> ~/.bashrc
 echo "#export ROS_MASTER_URI=http://jetosn:11311" >> ~/.bashrc
 echo "export ROS_MASTER_URI=http://$(hostname):11311" >> ~/.bashrc
+source ~/.bashrc
 
 sudo ~/osu-uwrt/riptide_software/src/riptide_hardware/scripts/add_rule
 echo "Please reboot your computer for final changes to take effect"
