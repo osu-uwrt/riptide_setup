f9fa959e87017fa1cfb788da66cb4f550c2b283f
tsender <sender.9@osu.edu>
2018-09-02 10:20:58 -0400

    FIxed acoustics install bug

--- riptide_utilities/install_ros/install_dependencies.sh
+++ riptide_utilities/install_ros/install_dependencies.sh
@@ -23,6 +23,7 @@ rm -rf ~/ceres-temp
 
 # Setup Acoustics
 tar -zxf ~/osu-uwrt/riptide_software/src/riptide_hardware/resources/acoustics/FrontPanel-Ubuntu16.04LTS-x64-5.0.1.tgz
+mv FrontPanel-Ubuntu16.04LTS-x64-5.0.1 ~
 cd ~/FrontPanel-Ubuntu16.04LTS-x64-5.0.1
 chmod +x install.sh
 sudo install.sh
