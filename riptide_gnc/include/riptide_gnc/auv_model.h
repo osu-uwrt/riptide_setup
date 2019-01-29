da36fbad72fde93d9ea28d74eb29b4a535bf4565
tsender <sender.9@osu.edu>
2019-01-29 18:24:36 -0500

    Wrote more of auv model

--- riptide_gnc/include/riptide_gnc/auv_model.h
+++ riptide_gnc/include/riptide_gnc/auv_model.h
@@ -9,21 +9,25 @@ using namespace std;
 
 typedef Matrix<float, 3, 2> Matrix32f;
 typedef Matrix<float, 6, 1> Vector6f;
+typedef Matrix<float, 5, 1> Vector5f;
 
 // AUV Model
 // Contains information about an AUV's attributes: inertia, volume, drag, and thruster properties
 class AUVModel
 {
 private:
-    float mass, volume;
+    float mass, volume, fluidRho;
+    Vector3f inertia;
     Matrix32f dragCoeffs;
-    vector<float[5]> thrusters;
+    vector<Vector5f> thrusters;
+    Vector3f rcob; // Center of buoyancy position relative to CoM
 public:
     const static float GRAVITY = 9.81; // [m/s^2]
-    const static float RHO = 1000; // [kg/m^3]
-    AUVModel(float m, float V, Matrix32f drag, vector<float[5]> auv_thrusters);
+    const static float DEFAULT_RHO = 1000; // [kg/m^3]
+    AUVModel(float m, Vector3f inertias, float V, float fluid_rho, vector3f cob, Matrix32f drag, vector<Vector5f> auv_thrusters);
     Vector6f DecomposeActuation(VectorXf actuation);
-    VectorXf DecomposeActuator(int thruster, float actuation);
+    Vector6f DecomposeActuator(int thruster, float actuation);
+    Vector6f DecomposeWeightForces(float phi, float theta);
 };
 
 #endif
--- riptide_gnc/src/auv_model.cpp
+++ riptide_gnc/src/auv_model.cpp
@@ -1,46 +1,94 @@
 #include "riptide_gnc/auv_model.h"
 
-AUModel::AUVModel(float m, float V, Matrix32f drag, vector<float[5]> auv_thrusters);
+AUModel::AUVModel(float m, Vector3f inertias, float V, float, fluid_rho, Vector3f cob,
+                  Matrix32f drag, vector<Vector5f> auv_thrusters);
 {
-    mass = m;
-    volume = V;
+    mass = m;             // [kg]
+    inertia = inertias;   // Assume only Jxx, Jyy, and Jzz
+    volume = V;           // [m^3]
+    fluidRho = fluid_rho; // [kg/m^3]
+    rcob = cob;
     dragCoeffs = drag;
     thrusters = auv_thrusters;
 }
 
-VectorXf AUVModel::DecomposeActuation(VectorXf actuation)
+// Get vector of ALL actuation forces/moments as expressed in the body frame
+Vector6f AUVModel::DecomposeActuation(VectorXf actuation)
 {
-    VectorXf output;
-    output.resize(6, 1);
+    Vector6f output;
     output.setZero();
 
     if (actuation.rows() == thrusters.size())
-    {
         for (int i = 0; i < thrusters.size(); i++)
-        {
-            output
-        }
-    }
+            output(i) = output(i) + AUVModel::DecomposeActuator(i, actuation(i));
+
     return output;
 }
 
+// Get actuator forces/moments as expressed in the body frame
+// Parameters:
+//      thruster = index of given thruster
+//      force = force from given thruster [N]
 VectorXf AUVModel::DecomposeActuator(int thruster, float force)
 {
     VectorXf output;
     output.resize(6, 1);
     output.setZero();
 
-    if (thruster < thrusters.size())
+    if (thruster < thrusters.size()) // Verify thruster index is valid
     {
-        float rx = thrusters[thruster][0];
-        float ry = thrusters[thruster][1];
-        float rz = thrusters[thruster][2];
-        float psi = thrusters[thruster][3];
-        float theta = thrusters[thruster][4];
-        
-        output(0) = force * cos(psi) * cos(theta);
-        output(1) = force * sin(psi) * cos(theta);
-        output(2) = -force * sin(theta);
+        Vector3f r, f, tao;
+        r.setZero();                          // Radius vector
+        f.setZero();                          // Force vector
+        tao.setZero();                        // Moment vector
+        float psi = thrusters[thruster](3);   // Yaw
+        float theta = thrusters[thruster](4); // Pitch
+
+        r = thrusters[thruster].head<3>();
+        f(0) = force * cos(psi) * cos(theta);
+        f(1) = force * sin(psi) * cos(theta);
+        f(2) = -force * sin(theta);
+        tao = r.cross(f);
+        output << f, t;
     }
-    return decomposition;
+
+    return output;
+}
+
+// Get forces/moments due to to vehicle's weight and buoyancy as expressed in the body frame
+// Parameters:
+//      phi = roll [rad]
+//      theta = pitch [rad]
+VectorXf AUVModel::DecomposeWeightForces(float phi, float theta)
+{
+    VectorXf output;
+    output.resize(6, 1);
+    output.setZero();
+
+    Vector3f w, fb, tao;
+    w.setZero();   // Weight vector
+    fb.setZero();  // Buoyant force vector
+    tao.setZero(); // Moment vector
+    float W = mass * GRAVITY;
+    float B = -fluidRho * volume * GRAVITY;
+
+    w(0) = -(W + B) * sin(theta);
+    w(1) = (W + B) * sin(phi) * cos(theta);
+    w(2) = (W + B) * cos(phi) * cos(theta);
+    fb(0) = -B * sin(theta);
+    fb(1) = B * sin(phi) * cos(theta);
+    fb(2) = B * cos(phi) * cos(theta);
+    tao = rcob.cross(fb);
+    output << w, tao;
+
+    return output;
+}
+
+// Compute vehicle acceleration expressed in the body-frame
+// Parameters:
+//   velBF = vel expressed in body-frame
+//   angVelBF = ang. vel expressed in the body-frame
+Vector3f AUVModel::BodyFrameAccel(Vector3f attitude, Vector3f velBF, Vector3f angVelBF)
+{
+
 }
