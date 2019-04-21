05586f0667cb3e4bd4e0db3ebb467af6885df294
Parth Parekh <prth.2000@gmail.com>
2019-04-21 11:07:45 -0400

    fixed setup.py

--- riptide_autonomy/src/runAutonomous.py
+++ riptide_autonomy/src/runAutonomous.py
@@ -8,3 +8,4 @@ import roslib
 class runAutonomous(smach.State) :
     def __init__(self): 
         smach.State.__init__(self, outcomes=['Gate Task','Path Markers', 'Nav', 'Buoy', 'Bin', 'Stake', 'Octagon', 'Error' ])
+           
