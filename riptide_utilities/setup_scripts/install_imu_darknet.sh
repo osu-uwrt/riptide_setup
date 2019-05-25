5e613e150e43ee6a2a13010e8736dd4f79f62e92
blaine141 <blaine141@gmail.com>
2019-05-25 14:28:03 -0400

    fixing bugs

--- riptide_utilities/setup_scripts/install_imu_darknet.sh
+++ riptide_utilities/setup_scripts/install_imu_darknet.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [[ $UID != 0 ]]; then
-	echo "Please run install_rosdeps.sh as root"
+	echo "Please run this as root"
 	exit 1
 fi
 
 cd ~/osu-uwrt/riptide_software/src/riptide_utilities/setup_scripts/packages/src
 git clone https://github.com/osu-uwrt/imu_3dm_gx4.git
-git clone https://github.com/osu-uwrt/darknet_ros.git
+git clone --recursive https://github.com/osu-uwrt/darknet_ros.git
 cd ..
 source /opt/ros/kinetic/setup.bash
 catkin_make -DCMAKE_INSTALL_PREFIX=/opt/ros/kinetic install
