b4fa79fab87436105269a4973a6ef90979245c31
blaine141 <blaine141@gmail.com>
2019-05-27 11:49:57 -0400

    Finished calibration

--- riptide_controllers/scripts/calibrationAction.py
+++ riptide_controllers/scripts/calibrationAction.py
@@ -28,19 +28,59 @@ class CalibrationAction(object):
         with open(rospy.get_param('~properties_file'), 'r') as stream:
             mass = yaml.safe_load(stream)['properties']['mass']
 
-        client.update_configuration({"Buoyant_Force": mass * 9.81, "Buoyancy_X_POS": 0, "Buoyancy_Y_POS": 0, "Buoyancy_Z_POS": 0})
+        Fb = mass * 9.81
+        CobX = 0
+        CobY = 0
+        CobZ = 0
+
+        client.update_configuration({"Buoyant_Force": Fb, "Buoyancy_X_POS": CobX, "Buoyancy_Y_POS": CobY, "Buoyancy_Z_POS": CobZ})
 
         self.depthPub.publish(True, 1)
+        att = AttitudeCommand()
+        att.roll_active = True
+        att.pitch_active = True
+        att.euler_rpy.x = 0
+        att.euler_rpy.y = 0
+        self.attitudePub.publish(att)
 
-        rospy.sleep(2)
-        for i in range(1,40)
-            force = rospy.wait_for_message("/command/force_depth", Vector3Stamped)
+        rospy.sleep(3)
+
+        for i in range(1, 20):
+            rospy.sleep(0.5)
+            forceMsg = rospy.wait_for_message("/command/force_depth", Vector3)
+            force = math.sqrt(forceMsg.x**2 + forceMsg.y**2 + forceMsg.z**2)
+
+            if forceMsg.z < 0:
+                force *= -1
+
+            Fb += force * 0.2
+            client.update_configuration({"Buoyant_Force": Fb, "Buoyancy_X_POS": CobX, "Buoyancy_Y_POS": CobY, "Buoyancy_Z_POS": CobZ})
+
+
+        for i in range(1, 20):
+            rospy.sleep(0.5)
+            momentMsg = rospy.wait_for_message("/command/moment", Vector3Stamped)
+            
+            CobY += momentMsg.vector.X / Fb * 0.2
+            CobX += momentMsg.vector.Y / Fb * 0.2
+
+            client.update_configuration({"Buoyant_Force": Fb, "Buoyancy_X_POS": CobX, "Buoyancy_Y_POS": CobY, "Buoyancy_Z_POS": CobZ})
+
+        att.euler_rpy.x = 90
+        self.attitudePub.publish(att)
+
+        rospy.sleep(3)
+
+        for i in range(1, 20):
+            rospy.sleep(0.5)
+            momentMsg = rospy.wait_for_message("/command/moment", Vector3Stamped)
+            
+            CobZ -= momentMsg.vector.X / Fb * 0.2
+
+            client.update_configuration({"Buoyant_Force": Fb, "Buoyancy_X_POS": CobX, "Buoyancy_Y_POS": CobY, "Buoyancy_Z_POS": CobZ})
+
             
 
-        # for i in range(1, 10):
-        #     time.sleep(6)
-        #     force = rospy.wait_for_message("/command/force_depth", Vector3)
-        #     self.fobPub.publish(math.sqrt(force.x**2 + force.y**2 + force.z**2))
         
         self._as.set_succeeded()
 
