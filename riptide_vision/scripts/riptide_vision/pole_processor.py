beb63a922c5624a1154565b293d9610ab8f42cd9
blaine141 <blaine141@gmail.com>
2019-06-05 23:09:05 -0400

    Started work with pole

--- riptide_vision/scripts/riptide_vision/pole_processor.py
+++ riptide_vision/scripts/riptide_vision/pole_processor.py
@@ -1,71 +1,51 @@
+#! /usr/bin/env python
+
 import rospy
+
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+
 from task_processor import TaskProcessor
 from riptide_msgs.msg import PoleData, BoundingBox
 from riptide_vision import RiptideVision
 from geometry_msgs.msg import Point
+from stereo_msgs.msg import DisparityImage
+from sensor_msgs.msg import Image
 
-# Class PoleProcessor
-# Inherits from TaskProcessor
-class PoleProcessor(TaskProcessor):
-    def __init__(self):
-        pole_pub = rospy.Publisher("/task/pole", PoleData, queue_size=1)
-        TaskProcessor.__init__(self, pole_pub)
 
-    # Normal Processing
-    def Process(self, image, debug_pub=None):
-        pole_msg = PoleData()
-        pole_msg.roll_correction = 0
-        pole_msg.beam_thickness = 0
-        pos = None
-        bbox = None
 
-        # Process the image
-        response = RiptideVision().detect_pole(image)
+def imgCB(msg):
+    global bridge
 
-        # Package data (if there is any)
-        if (len(response) > 2):
-            x_min = response[2]
-            y_min = response[3]
-            x_max = response[4]
-            y_max = response[5]
-            x_mid = response[6]
-            y_mid = response[7]
-            cam_center_x = response[8]
-            cam_center_y = response[9]
 
-            # Adjust all X and Y positions to be relative to the center of the camera frame
-            # Also maintain the axes convention in here (X is pos. to the right, Y is pos. down)
-            x_min = x_min - cam_center_x
-            x_max = x_max - cam_center_x
-            x_mid = x_mid - cam_center_x
+    try:
+        cv_image = bridge.imgmsg_to_cv2(msg.image)
+    except CvBridgeError as e:
+        print(e)
 
-            y_min = y_min - cam_center_y
-            y_max = y_max - cam_center_y
-            y_mid = y_mid - cam_center_y
+    (rows,cols) = cv_image.shape
+        
 
-            # pos = the middle of the pole in vehicle coordinate frame
-            pos = Point()
-            pos.x = 0
-            pos.y = -x_mid # Cam frame x negated
-            pos.z = -y_mid # Cam frame y negated
+    x = 0
+    maxScore = 0
+    for c in range(cols):
+        score = 0
+        for r in range(rows):
+            score += cv_image[r,c]
+        if (score > maxScore):
+            maxScore = score
+            x = c
 
-            bbox = BoundingBox()
-            bbox.top_left = Point(0, -x_min, -y_min)
-            bbox.bottom_right = Point(0, -x_max, -y_max)
+    cv2.line(cv_image, (x, rows), (x, 0), 255)
 
-            pole_msg.roll_correction = response[0]
-            pole_msg.beam_thickness = response[1]
-            image = response[10]
-        else:
-            pole_msg.roll_correction = response[0]
-            image = response[1]
+    try:
+        pub.publish(bridge.cv2_to_imgmsg(cv_image))
+    except CvBridgeError as e:
+        print(e)
 
-        if debug_pub is not None:
-            # Compress debug image
-            debug_img = RiptideVision().detect_pole_vis(image, response)
-            self.publish_debug_image(debug_img, debug_pub)
+rospy.init_node("pole_processor")
+rospy.Subscriber("/stereo/disparity", DisparityImage, imgCB)
+pub = rospy.Publisher("/debug/pole", Image, queue_size=5)
+bridge = CvBridge()
+rospy.spin()
 
-        # Publish data
-        self.task_pub.publish(pole_msg)
-
-        return pos, bbox
