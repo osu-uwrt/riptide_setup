ba0541dbd929a849c0119b86ea99da4a93c2c34c
juched <prth.2000@gmail.com>
2019-06-08 13:29:53 -0500

    added more to scripts

--- riptide_utilities/setup_scripts/install_imu_darknet.sh
+++ riptide_utilities/setup_scripts/install_imu_darknet.sh
@@ -6,11 +6,15 @@ mkdir dependencies
 cd dependencies
 mkdir src
 cd src
+
 git clone https://github.com/ros-drivers/pointgrey_camera_driver.git
 git clone https://github.com/osu-uwrt/imu_3dm_gx4.git
 git clone --recursive https://github.com/osu-uwrt/darknet_ros.git
 
 cd ..
+
+rosdep install --from-paths src --ignore-src --rosdistro kinetic -y -r
+
 source /opt/ros/kinetic/setup.bash
 catkin_make -DCMAKE_INSTALL_PREFIX=/opt/ros/kinetic install
 
