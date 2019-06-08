710d5de64f6cb28e638d2a3b0dcbb09c421935d7
juched <prth.2000@gmail.com>
2019-06-08 13:48:34 -0500

    trying to make the point-grey camera driver compile

--- riptide_utilities/setup_scripts/install_imu_darknet.sh
+++ riptide_utilities/setup_scripts/install_imu_darknet.sh
@@ -6,6 +6,13 @@ mkdir dependencies
 cd dependencies
 mkdir src
 cd src
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+apt-get update
+apt-get install python-rosdep -y
+'which rosdep' init
+rosdep update
+rosdep install --default-yes --from-paths ./ --rosdistro kinetic
+
 
 git clone https://github.com/ros-drivers/pointgrey_camera_driver.git
 git clone https://github.com/osu-uwrt/imu_3dm_gx4.git
