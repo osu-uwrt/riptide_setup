afcbccbcc0289a9e8a6bb338334d22a04c229337
blaine141 <blaine141@gmail.com>
2019-06-08 12:28:22 -0400

    Updated camera calibration

--- riptide_utilities/calibrate_camera_mono.sh
+++ riptide_utilities/calibrate_camera_mono.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Cameras must be running on the same machine as the calibrator
-roslaunch riptide_hardware cameras.launch
-rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.041 camera:=/downward image:=/downward/image_raw
-wait
-mv ~/.ros/camera_info ~/osu-uwrt/riptide_software/riptide_hardware/cfg/
+# Data will be saved to the computer the cameras are plugged into
+echo "Please ensure cameras are running (cameras.launch or underwaterCamera.launch)"
+#rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.041 camera:=/downward image:=/downward/image_raw
+rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.041 camera:=/underwater_cam image:=/underwater_cam/image_raw
+
