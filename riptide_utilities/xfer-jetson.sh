11ebf295153ce11e92b477ee9459e8ed5116e678
blaine141 <blaine141@gmail.com>
2019-06-11 19:20:51 -0400

    Getting pole processor to run on riptide

--- riptide_utilities/xfer-jetson.sh
+++ riptide_utilities/xfer-jetson.sh
@@ -4,3 +4,4 @@
 
 rsync -vrzc --delete --exclude=".*" --exclude=".*/" ~/osu-uwrt/riptide_software/src ros@jetson:~/osu-uwrt/riptide_software
 ssh ros@jetson 'chmod 700 ~/osu-uwrt/riptide_software/src/riptide_utilities/*.sh'
+ssh ros@riptide 'find ~/osu-uwrt/riptide_software/src/ -type f -iname "*.py" -exec chmod +x {} \;'
--- riptide_utilities/xfer-riptide.sh
+++ riptide_utilities/xfer-riptide.sh
@@ -4,3 +4,4 @@
 
 rsync -vrzc --delete --exclude=".*" --exclude=".*/" ~/osu-uwrt/riptide_software/src ros@riptide:~/osu-uwrt/riptide_software
 ssh ros@riptide 'chmod 700 ~/osu-uwrt/riptide_software/src/riptide_utilities/*.sh'
+ssh ros@riptide 'find ~/osu-uwrt/riptide_software/src/ -type f -iname "*.py" -exec chmod +x {} \;'
