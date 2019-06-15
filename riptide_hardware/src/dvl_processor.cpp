1f2d46f14d70e68aefd79bbc9366fef7a4064ea7
caffreyu <caffreyu@gmail.com>
2019-06-15 12:39:23 -0400

    Modified the dvl message

--- riptide_hardware/src/dvl_processor.cpp
+++ riptide_hardware/src/dvl_processor.cpp
@@ -49,33 +49,15 @@ void DVLProcessor::ImuCB(const riptide_msgs::Imu::ConstPtr &imu_msg)
   angular_vel(0) = imu_msg->ang_vel_rad.x;
   angular_vel(1) = imu_msg->ang_vel_rad.y;
   angular_vel(2) = imu_msg->ang_vel_rad.z;
-
-  /* Calculating without using the Eigen vector cross product
-  relative_vel(0) = angular_vel(1) * dvl_position(2) + angular_vel(2) * dvl_position(1);
-  relative_vel(1) = angular_vel(2) * dvl_position(0) + angular_vel(0) * dvl_position(2);
-  relative_vel(2) = angular_vel(0) * dvl_position(1) + angular_vel(1) * dvl_position(0);
-  */
    
-   relative_vel = angular_vel.cross(dvl_position);
+  relative_vel = angular_vel.cross(dvl_position);
 }
   
 void DVLProcessor::DvlCB(const nortek_dvl::Dvl::ConstPtr &dvl_msg)
 {
-  // state.header = dvl_msg->header;
-  state.vehicle_time = dvl_msg->time;
-  state.vehicle_dt1 = dvl_msg->dt1;
-  state.vehicle_dt2 = dvl_msg->dt2;
-
   state.vehicle_vel.x = dvl_msg->velocity.x - relative_vel(0);
   state.vehicle_vel.y = dvl_msg->velocity.y - relative_vel(1);
   state.vehicle_vel.z = dvl_msg->velocity.z - relative_vel(2);
-
-  state.vehicle_figureOfMerit = dvl_msg->figureOfMerit;
-  state.vehicle_beamDistance = dvl_msg->beamDistance;
-  state.vehicle_batteryVoltage = dvl_msg->batteryVoltage;
-  state.vehicle_speedSound = dvl_msg->speedSound;
-  state.vehicle_pressure = dvl_msg->pressure;
-  state.vehicle_temp = dvl_msg->temp;
 
   dvl_state_pub.publish(state);
 }
