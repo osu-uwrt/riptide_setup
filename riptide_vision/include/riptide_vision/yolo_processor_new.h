7ab48caa2358731f92b6c2573630d9ab82125d1d
caffreyu <caffreyu@gmail.com>
2019-07-13 18:53:26 -0400

    vision work for merge

--- riptide_vision/include/riptide_vision/yolo_processor_new.h
+++ riptide_vision/include/riptide_vision/yolo_processor_new.h
@@ -29,28 +29,32 @@ class YoloProcessorNew
   ros::Subscriber darknet_bbox_sub, image_sub, task_info_sub;
   ros::Publisher object_pub, low_detections_pub, task_image_pub;
 
-  darknet_ros_msgs::BoundingBoxes task_bboxes, low_detections;
-  string camera_topics[2] = {"/stereo/left/image_undistorted", "/downward/image_undistorted"};
+  darknet_ros_msgs::BoundingBoxes task_bboxes, low_detections, object_bbox;
+  string camera_topics[2] = {"/stereo/left/image_rect_color", "/downward/image_rect_color"};
+  riptide_msgs::Object object;
   cv_bridge::CvImagePtr cv_ptr;
   Mat task_image;
   int top_margin, num_rows, offset, text_start[4];
+  int width, height, cam_center_x, cam_center_y;
+  int task_id, num_tasks, alignment_plane, last_alignment_plane, num_objects, num_thresholds;
   vector<Scalar> colors;
   Scalar margin_color;
-
-  YAML::Node tasks;
-  string task_file, task_name;
-  int task_id, num_tasks, alignment_plane, last_alignment_plane, num_objects, num_thresholds;
+  geometry_msgs::Vector3 current_attitude;
+  string task_file, task_name, object_name;
+  vector<double> object_headings;
   vector<string> object_names;
   vector<double> thresholds;
 
  public:
-  YoloProcessor();
+  YoloProcessorNew();
   template <typename T>
   void LoadParam(string param, T &var);
   void UpdateTaskInfo();
   void ImageCB(const sensor_msgs::ImageConstPtr& msg);
   void DarknetBBoxCB(const darknet_ros_msgs::BoundingBoxes::ConstPtr& bbox_msg);
+  void TaskBBoxCB(const darknet_ros_msgs::BoundingBoxes::ConstPtr& bbox_msg);
   void TaskInfoCB(const riptide_msgs::TaskInfo::ConstPtr& task_msg);
+  void AlignmentCmdCB(const riptide_msgs::AlignmentCommand::ConstPtr& cmd);
 };
 
 #endif
