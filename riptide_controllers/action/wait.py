708ee3205d07f8e2881a3702ab922ee69a7200b9
caffreyu <caffreyu@gmail.com>
2019-07-13 21:16:12 -0400

    add wait action

---
+++ riptide_controllers/action/wait.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+import rospy
+import actionlib
+import dynamic_reconfigure.client
+
+from std_msgs import string, int32
+from darknet_ros_msgs.msg import BoundingBoxes
+
+import time
+
+# Do not need to import this yaml file
+# Also, need to add file path to import this one
+# import yaml
+# with open('yolov3-tiny_RS2018.yaml', 'r') as f:
+#     doc = yaml.load(f)
+
+
+class WaitAction(object):
+
+    def __init__(self):
+        self._as = actionlib.SimpleActionServer(
+            "wait", riptide_controllers.msg.WaitAction, execute_cb=self.execute_cb, auto_start=False)
+        self._as.start()
+
+    # def BBoxes_cb(self, msg):
+    #     count = 0
+    #     self.Objects.clear()
+    #     # Record all the classes in list Objects
+    #     while count < len(msg.bounding_boxes):
+    #         self.Objects.append(msg.bounding_boxes.index(count).Class)
+    #         count += 1
+    
+    def execute_cb(self, goal):
+        rospy.loginfo("Waiting for object %s", goal.object)
+        # Wait until you see the object a few times
+        count = 0
+        lastTime = time.time()
+        while count < goal.times:
+            boxes = rospy.wait_for_message("/state/bboxes", BoundingBoxes)
+            for a in boxes.bounding_boxes:
+                if a.Class == goal.object:
+                    if (time.time() - lastTime) < 0.3:
+                        count += 1
+                    else:
+                        count = 0
+            lastTime = time.time()
+                    
+        rospy.loginfo("Found object %s", goal.object)
+
+
+if __name__ == '__main__':
+    rospy.init_node('wait')
+    server = WaitAction()
+    rospy.spin()
