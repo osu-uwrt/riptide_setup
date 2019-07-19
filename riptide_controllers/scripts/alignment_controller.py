453efb97d44061871e7209973b118027763e309a
caffreyu <caffreyu@gmail.com>
2019-07-19 11:40:52 -0400

    modified alignment controller and cfg file

--- riptide_controllers/scripts/alignment_controller.py
+++ riptide_controllers/scripts/alignment_controller.py
@@ -3,8 +3,7 @@
 import rospy
 from riptide_msgs.msg import AlignmentCommand, DepthCommand, LinearCommand
 from darknet_ros_msgs.msg import BoundingBoxes
-from darknet_ros_msgs import BoundingBox
-from std_msgs.msg import Float32
+from std_msgs.msg import Float64
 from geometry_msgs.msg import Vector3
 from dynamic_reconfigure.server import Server
 from riptide_controllers.cfg import AlignmentControllerConfig
@@ -42,14 +41,14 @@ class AlignmentController():
         self.width_ratio = msg.width_ratio
         for bbox in msg.bounding_boxes:
             if self.object == bbox.Class:
-                self.x_error = ((bbox.xmin + bbox.xmax) / 2 - self.cam_width / 2)
-                self.y_error = ((bbox.ymin + bbox.ymax) / 2 - self.cam_height / 2)
-                self.z_error = ((bbox.xmax - bbox.xmin) - self.cam_width)
+                self.x_error = (bbox.xmin + bbox.xmax) / 2 - self.cam_width / 2
+                self.y_error = (bbox.ymin + bbox.ymax) / 2 - self.cam_height / 2
+                self.z_error = (bbox.xmax - bbox.xmin) - self.cam_width
         
-        self.x_forceCmd = X_FORCE_P * self.x_error
-        self.y_forceCmd = Y_FORCE_P * self.y_error
-        self.z_forceCmd = Z_FORCE_P * self.z_error
-        if self.cam == 0:
+        self.x_forceCmd = self.X_FORCE_P * self.x_error
+        self.y_forceCmd = self.Y_FORCE_P * self.y_error
+        self.z_forceCmd = self.Z_FORCE_P * self.z_error
+        if self.currentCam == 0:
             YPub.publish(self.x_forceCmd, LinearCommand.FORCE)
             XPub.publish(-self.z_forceCmd, LinearCommand.FORCE)
             depthPub.publish(-self.y_forceCmd, DepthCommand.depth)
@@ -59,10 +58,10 @@ class AlignmentController():
             depthPub.publish(-self.z_forceCmd, DepthCommand.depth)
 
     def reconfigure(self, config, name):
-        self.X_FORCE_P = config[name + "x_force_p"]
-        self.Y_FORCE_P = config[name + "y_force_p"]
-        self.Z_FORCE_P = config[name + "z_force_p"]
-        self.MAX_FORCE = config[name + "max_force"]
+        self.X_FORCE_P = config[name + "_x_force_p"]
+        self.Y_FORCE_P = config[name + "_y_force_p"]
+        self.Z_FORCE_P = config[name + "_z_force_p"]
+        self.MAX_FORCE = config[name + "_max_force"]
         
 alignmentController = AlignmentController()
 
