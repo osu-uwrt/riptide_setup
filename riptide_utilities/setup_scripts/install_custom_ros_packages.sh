6ad93d9c00e19354dbdbd72c757ca9e4de585371
juched <prth.2000@gmail.com>
2019-11-07 19:26:47 +0000

    fixed setup script

--- riptide_utilities/setup_scripts/install_custom_ros_packages.sh
+++ riptide_utilities/setup_scripts/install_custom_ros_packages.sh
@@ -17,11 +17,9 @@ rosdep install pointgrey_camera_driver
 git clone https://github.com/ros-drivers/pointgrey_camera_driver.git
 git clone https://github.com/osu-uwrt/imu_3dm_gx4.git
 git clone --recursive https://github.com/osu-uwrt/darknet_ros.git
-<<<<<<< HEAD:riptide_utilities/setup_scripts/install_imu_darknet.sh
 
-=======
 git clone https://github.com/osu-uwrt/nortek_dvl.git
->>>>>>> 1b380889c063b8866bc356f586752b63b1a429f8:riptide_utilities/setup_scripts/install_custom_ros_packages.sh
+
 cd ..
 
 rosdep install --from-paths src --ignore-src --rosdistro kinetic -y -r
