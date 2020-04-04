3590b9110fcce3fdf413cfec396a51f67e81287e
Blaine Miller <blaine141@gmail.com>
2020-04-04 15:12:39 -0400

    Using pip instead

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
-sudo apt-get update
-sudo apt-get install -y python3-vcstool 
+sudo pip install vcstool
 
 if [ ! -d "../riptide_software/src" ]; then
     mkdir -p ../riptide_software
