f715f60052ffc9d05cd1e472375db2e06d9ef8c0
Blaine Miller <blaine141@gmail.com>
2020-04-05 00:36:27 -0400

    Install flexbe app

--- scripts/setup_scripts/install_custom_ros_packages.sh
+++ scripts/setup_scripts/install_custom_ros_packages.sh
@@ -13,6 +13,7 @@ fi
 git -C imu_3dm_gx4 pull || git clone https://github.com/osu-uwrt/imu_3dm_gx4.git
 git -C darknet_ros pull --recurse-submodules || git clone --recursive https://github.com/osu-uwrt/darknet_ros.git
 git -C nortek_dvl pull || git clone https://github.com/osu-uwrt/nortek_dvl.git
+git -C flexbe_app pull || https://github.com/FlexBE/flexbe_app.git
 cd ..
 
 rosdep install --from-paths src --ignore-src --rosdistro $ROS_DISTRO -y -r
