db3f4835fb1c9bdd80ccaa492eb31affdced150d
Anden Acitelli <a.acitelli@gmail.com>
2020-08-28 11:03:56 -0400

    Add pip install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Default Linux installation doesn't have pip. Install and update for both Python versions for good measure
+sudo apt-get install python-pip python3-pip --yes
+sudo python3 -m pip install pip --upgrade
+sudo python -m pip install pip --upgrade
+
 sudo pip install vcstool
 
 if [ ! -d "../riptide_software/src" ]; then
