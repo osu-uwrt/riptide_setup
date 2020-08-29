d62cf8109553facc8d782ed2f0a451ebd5c704dc
Anden Acitelli <a.acitelli@gmail.com>
2020-08-29 15:21:40 -0400

    fix pip hang (sudo permission does weird things)

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 # Default Linux installation doesn't have pip. Install and update for both Python versions for good measure
 sudo apt-get install python-pip python3-pip --yes
-sudo python3 -m pip install pip --upgrade
-sudo python -m pip install pip --upgrade
+python3 -m pip install pip --upgrade
+python -m pip install pip --upgrade
 
-sudo pip install vcstool
+pip install vcstool
 
 if [ ! -d "../riptide_software/src" ]; then
     mkdir -p ../riptide_software
