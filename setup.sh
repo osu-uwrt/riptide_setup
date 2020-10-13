fc032fc0f335f9af913a1fd38d75460e99ca2de2
Blaine Miller <blaine141@gmail.com>
2020-10-13 13:08:00 -0400

    Fixing pip and vscode install

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # Default Linux installation doesn't have pip. Install and update for both Python versions for good measure
 sudo apt-get install python-pip python3-pip --yes
 python -m pip install --upgrade pip
-python3 -m pip install --upgrade pip3 
-pip install vcstool
+python3 -m pip install --upgrade pip
+sudo python -m pip install vcstool
 
 if [ ! -d "../riptide_software/src" ]; then
     mkdir -p ../riptide_software
