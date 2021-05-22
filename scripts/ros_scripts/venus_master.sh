cc9b57c4d5ad2eebe715481b754f7343697ebcef
OSU UWRT <osu.uwrt@gmail.com>
2021-05-22 12:50:34 -0400

    Changing puddles computer to venus

--- scripts/ros_scripts/venus_master.sh
+++ scripts/ros_scripts/venus_master.sh
@@ -2,6 +2,6 @@
 
 source /opt/ros/noetic/setup.bash
 source ~/osu-uwrt/riptide_software/devel/setup.bash
-export ROS_MASTER_URI=http://baycat:11311
+export ROS_MASTER_URI=http://venus:11311
 
 exec "$@"
--- scripts/setup_scripts/setup_hosts.sh
+++ scripts/setup_scripts/setup_hosts.sh
@@ -6,8 +6,8 @@ if ! grep -Fq "baycat" /etc/hosts; then
     echo -e "192.168.1.19\tbaycat\n$(cat /etc/hosts)" > /etc/hosts
 fi
 
-if ! grep -Fq "riptide" /etc/hosts; then
-    echo -e "192.168.1.16\triptide\n$(cat /etc/hosts)" > /etc/hosts
+if ! grep -Fq "venus" /etc/hosts; then
+    echo -e "192.168.1.21\tvenus\n$(cat /etc/hosts)" > /etc/hosts
 fi
 
 if ! grep -Fq "jetson" /etc/hosts; then
