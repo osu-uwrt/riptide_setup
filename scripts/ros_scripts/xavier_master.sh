13051d1d2c836fe2ac7daf934f2d432083dafbf0
mitchsayre <sketch0306@gmail.com>
2022-01-18 17:00:33 -0500

    Add xavier support

---
+++ scripts/ros_scripts/xavier_master.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /opt/ros/noetic/setup.bash
+source ~/osu-uwrt/riptide_software/devel/setup.bash
+export ROS_MASTER_URI=http://xavier:11311
+
+exec "$@"
--- scripts/setup_scripts/setup_hosts.sh
+++ scripts/setup_scripts/setup_hosts.sh
@@ -2,6 +2,10 @@
 
 # Update /etc/hosts File
 # Add appropriate lines to the hosts if they do not exist
+if ! grep -Fq "xavier" /etc/hosts; then
+    echo -e "192.168.1.22\txavier\n$(cat /etc/hosts)" > /etc/hosts
+fi
+
 if ! grep -Fq "baycat" /etc/hosts; then
     echo -e "192.168.1.19\tbaycat\n$(cat /etc/hosts)" > /etc/hosts
 fi
