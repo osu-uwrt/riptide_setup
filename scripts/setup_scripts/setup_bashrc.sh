bfd5a7454d512a651ac2cb9f86ff2cb62c7c8ac5
coalman321 <cjtucker321@gmail.com>
2022-01-30 13:33:46 -0500

    fix bashrc

--- scripts/setup_scripts/setup_bashrc.sh
+++ scripts/setup_scripts/setup_bashrc.sh
@@ -4,7 +4,7 @@
 
 s1="source /opt/ros/$ROS_DISTRO/setup.bash"
 s2="source ~/osu-uwrt/dependencies/install/setup.bash"
-s3="source ~/osu-uwrt/riptide_software/devel/setup.bash"
+s3="source ~/osu-uwrt/riptide_software/install/setup.bash"
 
 # Add appropriate lines to the bashrc if they do not exist
 if ! grep -q "$s1" ~/.bashrc || ! grep -q "$s2" ~/.bashrc || ! grep -q "$s3" ~/.bashrc; then
