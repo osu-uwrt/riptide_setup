9ebc45c190312aeef9946594b19d8c68ec8896b5
mitchsayre <sketch0306@gmail.com>
2022-07-11 22:23:18 -0400

    make xfer a bit better

---
+++ scripts/ros_scripts/build_remote.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then 
+    echo "missing directory argument"
+    exit -1
+fi
+if [ ! -d "$1" ]; then 
+    echo "Build called on a non-existant directory"
+    echo "$1"
+    exit -2
+fi
+
+source /opt/ros/galactic/setup.bash
+
+if [ ! "$1" == *"dependencies"* ]; then
+    echo "Sourcing riptide deps"
+    source /home/ros/osu-uwrt/dependencies/install/setup.bash
+fi
+
+cd $1
+
+if [[ $# -eq 2 && $2 == "--symlink-install" ]]; then
+    echo "Running symlink build"
+    colcon build --symlink-install
+else
+    echo "Running full build"
+    colcon build
+fi
--- scripts/ros_scripts/xfer.sh
+++ scripts/ros_scripts/xfer.sh
@@ -1,4 +1,21 @@
 ~/osu-uwrt/riptide_setup/scripts/dates_scripts/date_set.sh $1
 
-rsync -vrzc --delete --exclude="**/.git/" --exclude="riptide_gazebo/" --exclude="riptide_vision/weights/" ~/osu-uwrt/riptide_software/src ros@$1:~/osu-uwrt/riptide_software
-ssh ros@$1 'find ~/osu-uwrt/riptide_software/src/ -type f -iname "*.py" -exec chmod +x {} \;'
+scp ~/osu-uwrt/riptide_setup/scripts/ros_scripts/build_remote.sh ros@xavier:~/osu-uwrt
+
+echo "xfering robot code"
+rsync -vrzc --delete --exclude="**/.git/" --exclude="riptide_gazebo/" --exclude="**/.vscode/" --exclude="riptide_rqt_plugins" --exclude="riptide_vision/weights/" ~/osu-uwrt/riptide_software/src ros@$1:~/osu-uwrt/riptide_software
+
+if [ $# -eq 2 ]; then 
+    if [ $2 == "--deps" ]; then 
+        echo "xfering dependencies"
+
+        rsync -vrzc --delete --exclude="**/.git/" --exclude="riptide_gazebo/" --exclude="**/.vscode/" --exclude="riptide_rqt_plugins" --exclude="riptide_vision/weights/" ~/osu-uwrt/dependencies/src ros@$1:~/osu-uwrt/dependencies
+
+        echo "Running a symlink build on dependencies"
+        ssh ros@xavier 'cd ~/osu-uwrt && ./build_remote.sh ./dependencies --symlink-install'
+    fi
+    if [ $2 == "--build" ]; then
+        echo "Running a symlink build on riptide_software"
+        ssh ros@xavier 'cd ~/osu-uwrt && ./build_remote.sh ./riptide_software --symlink-install'
+    fi
+fi
