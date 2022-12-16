6b47a813a7e2d56cf593d66321826559e0251967
coalman321 <cjtucker321@gmail.com>
2022-12-15 19:39:54 -0500

    a bit closer

---
+++ setup_scripts/install_ros.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+ROS_DISTRO="humble"
+
+#Getting version and release number of Ubuntu
+version=`lsb_release -sc`
+releasenum=`grep DISTRIB_DESCRIPTION /etc/*-release | awk -F 'Ubuntu ' '{print $2}' | awk -F ' LTS' '{print $1}'`
+echo ">>> {Your Ubuntu version is: [Ubuntu $version $releasenum]}"
+
+
+# add ros key to keyring
+sudo apt update && sudo apt install curl gnupg lsb-release -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+# add ros2 repos to the sources.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] \
+ http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" \
+ | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+
+sudo apt update
+
+echo "#######################################################################################################################"
+echo ""
+echo ">>>  {Starting ROS installation, this will take about 20 min. It will depends on your internet  connection}"
+echo ""
+
+read -p "Enter your install (Default is desktop):" answer 
+
+if [ "$answer" == "base" ]; then # base install
+
+  sudo apt install ros-${ROS_DISTRO}-ros-base -y
+
+else # Desktop install 
+
+  sudo apt install ros-${ROS_DISTRO}-desktop -y
+
+fi
+
+# install ros2 tools
+sudo apt install python3-rosdep python3-colcon-common-extensions -y 
+
+sudo rosdep init
+rosdep update
--- setup_scripts/setup_hosts.sh
+++ setup_scripts/setup_hosts.sh
@@ -2,16 +2,12 @@
 
 # Update /etc/hosts File
 # Add appropriate lines to the hosts if they do not exist
-if ! grep -Fq "baycat" /etc/hosts; then
-    echo -e "192.168.1.19\tbaycat\n$(cat /etc/hosts)" > /etc/hosts
+if ! grep -Fq "xavier" /etc/hosts; then
+    echo -e "192.168.1.69\txavier\n$(cat /etc/hosts)" > /etc/hosts
 fi
 
-if ! grep -Fq "venus" /etc/hosts; then
-    echo -e "192.168.1.21\tvenus\n$(cat /etc/hosts)" > /etc/hosts
-fi
-
-if ! grep -Fq "jetson" /etc/hosts; then
-    echo -e "192.168.1.69\tjetson\n$(cat /etc/hosts)" > /etc/hosts
+if ! grep -Fq "orin" /etc/hosts; then
+    echo -e "192.168.1.69\torin\n$(cat /etc/hosts)" > /etc/hosts
 fi
 
 if ! grep -Fq "uwrt-dvl" /etc/hosts; then
